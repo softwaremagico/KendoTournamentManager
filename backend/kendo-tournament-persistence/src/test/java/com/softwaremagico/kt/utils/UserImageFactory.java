3297395c348743749f5b81156949e8b476e9d90e
Software Magico <softwaremagico@gmail.com>
2021-12-27 16:41:06 +0100

    Added User and UserImage

--- backend/kendo-tournament-persistence/src/test/java/com/softwaremagico/kt/utils/UserImageFactory.java
+++ backend/kendo-tournament-persistence/src/test/java/com/softwaremagico/kt/utils/UserImageFactory.java
@@ -1,8 +1,7 @@
 package com.softwaremagico.kt.utils;
 
-import com.softwaremagico.kt.persistence.entities.ImageType;
-import com.softwaremagico.kt.persistence.entities.User;
 import com.softwaremagico.kt.persistence.entities.UserImage;
+import com.softwaremagico.kt.persistence.entities.User;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Files;
@@ -11,13 +10,12 @@ import java.nio.file.Paths;
 @Service
 public class UserImageFactory {
 
-    public UserImage createUserImage(String resource, ImageType type, User user) throws Exception {
+    public UserImage createUserImage(String resource, User user) throws Exception {
         byte[] image = Files.readAllBytes(Paths.get(getClass().getClassLoader()
                 .getResource(resource).toURI()));
         UserImage userImage = new UserImage();
         userImage.setUser(user);
         userImage.setData(image);
-        userImage.setType(type);
 
         return userImage;
     }
