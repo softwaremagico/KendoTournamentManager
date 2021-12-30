4e9fdffc2edac528f47a6ac012eaa8476d71ffb7
Jorge Hortelano <jhortelano@biit-solutions.com>
2021-12-30 10:10:46 +0100

    Project generation

---
+++ frontend/src/app/models/club.ts
@@ -0,0 +1,11 @@
+export class Club {
+  public id?: number;
+  public name: string;
+  public country?: string;
+  public city?: string;
+  public address?: string;
+  public email?: string;
+  public phone?: string;
+  public web?: string;
+
+}
