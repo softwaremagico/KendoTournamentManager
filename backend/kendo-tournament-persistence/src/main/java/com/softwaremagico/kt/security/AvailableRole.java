88b5320a61b09277c5ac95186fd685c73feb0493
Jorge Hortelano <jhortelano@biit-solutions.com>
2021-12-30 11:43:32 +0100

    Adding roles for authenticated users

---
+++ backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/security/AvailableRole.java
@@ -0,0 +1,17 @@
+package com.softwaremagico.kt.security;
+
+public enum AvailableRole {
+    ROLE_VIEWER,
+    ROLE_ADMIN;
+
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    public static AvailableRole get(String roleName) {
+        for (AvailableRole availableRole : AvailableRole.values()) {
+            if (availableRole.name().equalsIgnoreCase(ROLE_PREFIX + roleName)) {
+                return availableRole;
+            }
+        }
+        return null;
+    }
+}
