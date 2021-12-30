15f070f137f1a3a43d6a30429a47dbb96390a9fb
Jorge Hortelano <jhortelano@biit-solutions.com>
2021-12-30 12:14:37 +0100

    Added services for users

---
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/UserServices.java
@@ -0,0 +1,56 @@
+package com.softwaremagico.kt.rest;
+
+import com.softwaremagico.kt.core.providers.UserProvider;
+import com.softwaremagico.kt.persistence.entities.User;
+import com.softwaremagico.kt.rest.model.UserDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/users")
+public class UserServices {
+    private final UserProvider userProvider;
+    private final ModelMapper modelMapper;
+
+    public UserServices(UserProvider userProvider, ModelMapper modelMapper) {
+        this.userProvider = userProvider;
+        this.modelMapper = modelMapper;
+    }
+
+    @PreAuthorize("hasRole('ROLE_VIEWER')")
+    @ApiOperation(value = "Gets a user.")
+    @PostMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@ApiParam(value = "Id of an existing user", required = true) @PathParam("id") Integer id,
+                    HttpServletRequest request) {
+        return userProvider.get(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Creates a user.")
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@RequestBody UserDto user, HttpServletRequest request) {
+        userProvider.add(modelMapper.map(user, User.class));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Deletes a user.")
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@ApiParam(value = "Id of an existing user", required = true) @PathParam("id") Integer id,
+                       HttpServletRequest request) {
+        userProvider.delete(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Updates a user.")
+    @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody UserDto user, HttpServletRequest request) {
+        userProvider.update(modelMapper.map(user, User.class));
+    }
+}
---
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/model/UserDto.java
@@ -0,0 +1,44 @@
+package com.softwaremagico.kt.rest.model;
+
+public class UserDto {
+
+    private Integer id;
+
+    private String idCard;
+
+    private String name = "";
+
+    private String lastname = "";
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
