5858ac615603938e632b28a44c082ab58cd0b956
Jorge Hortelano <jhortelano@biit-solutions.com>
2021-12-30 11:44:03 +0100

    Basic Club information exchange

---
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/ClubServices.java
@@ -0,0 +1,66 @@
+package com.softwaremagico.kt.rest;
+
+import com.softwaremagico.kt.core.providers.ClubProvider;
+import com.softwaremagico.kt.persistence.entities.Club;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/clubs")
+public class ClubServices {
+    private final ClubProvider clubProvider;
+    private final ModelMapper modelMapper;
+
+    public ClubServices(ClubProvider clubProvider, ModelMapper modelMapper) {
+        this.clubProvider = clubProvider;
+        this.modelMapper = modelMapper;
+    }
+
+    @PreAuthorize("hasRole('ROLE_VIEWER')")
+    @ApiOperation(value = "Creates a club with some basic information.")
+    @PostMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Club get(@ApiParam(value = "Id of an existing club", required = true) @PathParam("id") Integer id,
+                    HttpServletRequest request) {
+        return clubProvider.get(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Creates a club with some basic information.")
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@ApiParam(value = "Name of the new club", required = true) @RequestParam(name = "name") String name,
+                    @ApiParam(value = "Country where the club is located", required = true) @RequestParam(name = "country") String country,
+                    @ApiParam(value = "City where the club is located", required = true) @RequestParam(name = "city") String city,
+                    HttpServletRequest request) {
+        clubProvider.add(name, country, city);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Creates a club with full information.")
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@RequestBody ClubDto club, HttpServletRequest request) {
+        clubProvider.add(modelMapper.map(club, Club.class));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Deletes a club.")
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@ApiParam(value = "Id of an existing club", required = true) @PathParam("id") Integer id,
+                       HttpServletRequest request) {
+        clubProvider.delete(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Updates a club.")
+    @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@ApiParam(value = "Id of an existing club", required = true) @PathParam("id") Integer id,
+                       HttpServletRequest request) {
+        clubProvider.delete(id);
+    }
+}
---
+++ frontend/src/app/services/club.service.ts
@@ -0,0 +1,65 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {EnvironmentService} from "../environment.service";
+import {catchError, map, tap} from 'rxjs/operators';
+import {Observable, of} from "rxjs";
+import {Club} from "../models/club";
+import {LoggerService} from "../logger.service";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClubService {
+
+  private baseUrl = this.environmentService.getBackendUrl() + 'clubs';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private http: HttpClient, private environmentService: EnvironmentService, private loggerService: LoggerService) {
+  }
+
+  get(id: number): Observable<Club> {
+    const url: string = `${this.baseUrl}/${id}`;
+    return this.http.get<Club>(url).pipe(
+      tap(_ => this.log(`fetched club id=${id}`)),
+      catchError(this.handleError<Club>(`get id=${id}`))
+    );
+  }
+
+  delete(id: number) {
+    const url: string = `${this.baseUrl}/${id}`;
+    this.http.delete(url).pipe(
+      tap(_ => this.log(`deleting club id=${id}`)),
+      catchError(this.handleError<Club>(`delete id=${id}`))
+    );
+  }
+
+  add(club: Club): Observable<Club> {
+    const url: string = `${this.baseUrl}`;
+    return this.http.post<Club>(url, club, this.httpOptions).pipe(
+      tap((newClub: Club) => this.log(`adding club ${newClub}`)),
+      catchError(this.handleError<Club>(`adding ${club}`))
+    );
+  }
+
+  private log(message: string) {
+    this.loggerService.add(`ClubService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
