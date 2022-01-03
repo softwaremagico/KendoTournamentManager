fbc550ac61326588a362f9c5396508e01e2e61df
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-01-03 10:22:05 +0100

    Added responsive menu

---
+++ frontend/src/app/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-menu',
+  templateUrl: './navigation-menu.component.html',
+  styleUrls: ['./navigation-menu.component.css']
+})
+export class NavigationMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
