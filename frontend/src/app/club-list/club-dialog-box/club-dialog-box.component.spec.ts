8a7f451be2642831c8d7cf3f41852ed21b65ac46
Software Magico <softwaremagico@gmail.com>
2022-01-03 14:42:00 +0100

    Moved Club Dialog to inside compontent

--- frontend/src/app/club-list/club-dialog-box/club-dialog-box.component.spec.ts
+++ frontend/src/app/club-list/club-dialog-box/club-dialog-box.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DialogBoxComponent } from './dialog-box.component';
+import { ClubDialogBoxComponent } from './club-dialog-box.component';
 
 describe('DialogBoxComponent', () => {
-  let component: DialogBoxComponent;
-  let fixture: ComponentFixture<DialogBoxComponent>;
+  let component: ClubDialogBoxComponent;
+  let fixture: ComponentFixture<ClubDialogBoxComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DialogBoxComponent ]
+      declarations: [ ClubDialogBoxComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DialogBoxComponent);
+    fixture = TestBed.createComponent(ClubDialogBoxComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
