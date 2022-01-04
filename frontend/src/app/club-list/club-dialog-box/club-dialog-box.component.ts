0a9de9a6aba2a6563a50dcb9551c87634d79cbf3
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-01-04 10:49:27 +0100

    Updating modal window

--- frontend/src/app/club-list/club-dialog-box/club-dialog-box.component.ts
+++ frontend/src/app/club-list/club-dialog-box/club-dialog-box.component.ts
@@ -17,23 +17,26 @@ export enum Action {
 export class ClubDialogBoxComponent {
 
   club: Club;
+  title: string;
   action: Action;
-  ActionEnum: typeof Action = Action;
+  actionName: string;
 
   constructor(
     public dialogRef: MatDialogRef<ClubDialogBoxComponent>,
     //@Optional() is used to prevent error if no data is passed
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: Club, action: Action) {
-    this.club = data;
-    this.action = action;
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: { title: string, action: Action, entity: Club }) {
+    this.club = data.entity;
+    this.title = data.title;
+    this.action = data.action;
+    this.actionName = Action[data.action];
   }
 
   doAction() {
-    this.dialogRef.close({data: this.club});
+    this.dialogRef.close({data: this.club, action: this.action});
   }
 
   closeDialog() {
-    this.dialogRef.close(Action.Cancel);
+    this.dialogRef.close({action: this.action});
   }
 
 }
