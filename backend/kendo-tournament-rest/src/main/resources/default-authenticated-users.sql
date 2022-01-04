c8caf873f3456b0ea14185477c9e049d66cb3f33
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-01-04 15:57:45 +0100

    Correctly sending JWT token to the server.

--- backend/kendo-tournament-rest/src/main/resources/default-authenticated-users.sql
+++ backend/kendo-tournament-rest/src/main/resources/default-authenticated-users.sql
@@ -1,1 +1,3 @@
-INSERT INTO authenticated_users (username, password, full_name) VALUES ('admin@test.com', '$2a$12$hawW3GfY4/Ib/1.9KdVvVObw2t4FsXjkYApy5xlJf.P5GO3K72OSm', 'Admin User');
+INSERT INTO authenticated_users (id, username, password, full_name) VALUES (1, 'admin@test.com', '$2a$12$hawW3GfY4/Ib/1.9KdVvVObw2t4FsXjkYApy5xlJf.P5GO3K72OSm', 'Admin User');
+INSERT INTO roles (authenticated_user, roles) VALUES (1, 'admin');
+INSERT INTO roles (authenticated_user, roles) VALUES (1, 'viewer');
