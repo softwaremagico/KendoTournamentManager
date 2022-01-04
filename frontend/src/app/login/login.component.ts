533651782216e1cb893e23fc4a34a752f4388f92
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-01-04 15:32:25 +0100

    Login redirects to previous screen.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 import {AuthenticatedUserService} from "../services/authenticated-user.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,17 @@ export class LoginComponent {
   email: string;
   password: string;
 
-  constructor(public authenticatedUserService: AuthenticatedUserService) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, public authenticatedUserService: AuthenticatedUserService) {
   }
 
   login() {
     this.authenticatedUserService.login(this.email, this.password).subscribe(data => {
       this.authenticatedUserService.setToken(data.jwt);
+
+      this.activatedRoute.queryParams.subscribe((params: any) => {
+        const returnUrl = params['returnUrl'];
+        this.router.navigate([returnUrl]);
+      });
     });
   }
 }
