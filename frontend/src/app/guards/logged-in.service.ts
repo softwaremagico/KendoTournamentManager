ad981367a955670e0e5ee4e61478561ad40fd393
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-01-04 15:32:25 +0100

    Login redirects to previous screen.

--- frontend/src/app/guards/logged-in.service.ts
+++ frontend/src/app/guards/logged-in.service.ts
@@ -1,17 +1,18 @@
 import {Injectable} from '@angular/core';
 import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {AuthenticatedUserService} from "../services/authenticated-user.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedInService implements CanActivate {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public authenticatedUserService: AuthenticatedUserService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (localStorage.getItem('currentUser')) {
-      // logged in so return true
+    if (this.authenticatedUserService.getToken()) {
+      // JWT Token exists, is a registered user.
       return true;
     }
 
