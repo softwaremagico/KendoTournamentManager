c4f9f85ebd846ca6692ba6aa932fbcc5b6916e63
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-01-05 10:45:00 +0100

    Added translations

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Kendo Tournament Generator v2';
+  selectedLanguage = 'en';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'es', 'it', 'de', 'nl', 'ca']);
+    translate.setDefaultLang('en');
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+    this.selectedLanguage = lang;
+  }
 }
