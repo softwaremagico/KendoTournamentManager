8bf331bb2206d4265a05aede252ed33aa4161250
Software Magico <softwaremagico@gmail.com>
2022-01-07 16:38:20 +0100

    Hidding menu if is not a logged user.

--- frontend/src/app/guards/logged-in.service.ts
+++ frontend/src/app/guards/logged-in.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
 import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {AuthenticatedUserService} from "../services/authenticated-user.service";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedInService implements CanActivate {
+
+  public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private router: Router, public authenticatedUserService: AuthenticatedUserService) {
   }
@@ -13,11 +16,13 @@ export class LoggedInService implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.authenticatedUserService.getJwtValue()) {
       // JWT Token exists, is a registered user.
+      this.isUserLoggedIn.next(true);
       return true;
     }
 
     // not logged in so redirect to login page with the return url
     this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+    this.isUserLoggedIn.next(false);
     return false;
   }
 }
