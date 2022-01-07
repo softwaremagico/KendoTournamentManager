d4d77c60f26d82175428d9925ab5b2353591d62f
Software Magico <softwaremagico@gmail.com>
2022-01-07 16:43:14 +0100

    Storing selected language.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {AuthenticatedUserService} from "./services/authenticated-user.service";
 import {LoggedInService} from "./guards/logged-in.service";
+import {UserSessionService} from "./services/user-session.service";
 
 @Component({
   selector: 'app-root',
@@ -14,14 +15,20 @@ export class AppComponent {
   selectedLanguage = 'en';
   loggedIn = false;
 
-  constructor(public translate: TranslateService, public authenticatedUserService: AuthenticatedUserService, public loggedInService: LoggedInService) {
+  constructor(public translate: TranslateService, public authenticatedUserService: AuthenticatedUserService, public loggedInService: LoggedInService,
+              private userSessionService: UserSessionService) {
     translate.addLangs(['en', 'es', 'it', 'de', 'nl', 'ca']);
     translate.setDefaultLang('en');
     this.loggedInService.isUserLoggedIn.subscribe(value => this.loggedIn = value);
+    if (userSessionService.getLanguage()) {
+      this.translate.use(userSessionService.getLanguage());
+      this.selectedLanguage = userSessionService.getLanguage();
+    }
   }
 
   switchLanguage(lang: string) {
     this.translate.use(lang);
     this.selectedLanguage = lang;
+    this.userSessionService.setLanguage(lang);
   }
 }
