bb70b468387a63bbe29313b47aae415782c775bd
Software Magico <softwaremagico@gmail.com>
2022-01-16 17:48:15 +0100

    Setting value club

--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/ParticipantServices.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/ParticipantServices.java
@@ -8,23 +8,25 @@ package com.softwaremagico.kt.rest;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
+import com.softwaremagico.kt.core.providers.ClubProvider;
 import com.softwaremagico.kt.core.providers.ParticipantProvider;
+import com.softwaremagico.kt.persistence.entities.Club;
 import com.softwaremagico.kt.persistence.entities.Participant;
 import com.softwaremagico.kt.rest.model.ParticipantDto;
 import io.swagger.annotations.ApiOperation;
@@ -43,10 +45,12 @@ import java.util.List;
 @RequestMapping("/participants")
 public class ParticipantServices {
     private final ParticipantProvider participantProvider;
+    private final ClubProvider clubProvider;
     private final ModelMapper modelMapper;
 
-    public ParticipantServices(ParticipantProvider participantProvider, ModelMapper modelMapper) {
+    public ParticipantServices(ParticipantProvider participantProvider, ClubProvider clubProvider, ModelMapper modelMapper) {
         this.participantProvider = participantProvider;
+        this.clubProvider = clubProvider;
         this.modelMapper = modelMapper;
     }
 
@@ -69,8 +73,16 @@ public class ParticipantServices {
     @ApiOperation(value = "Creates a participant.")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Participant add(@RequestBody ParticipantDto participant, HttpServletRequest request) {
-        return participantProvider.add(modelMapper.map(participant, Participant.class));
+    public Participant add(@RequestBody ParticipantDto participantDto, HttpServletRequest request) {
+        final Club club;
+        if (participantDto.getClub() != null) {
+            club = clubProvider.get(participantDto.getClub().getId());
+        } else {
+            club = null;
+        }
+        final Participant participant = modelMapper.map(participantDto, Participant.class);
+        participant.setClub(club);
+        return participantProvider.add(participant);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -93,7 +105,15 @@ public class ParticipantServices {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiOperation(value = "Updates a participant.")
     @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Participant update(@RequestBody ParticipantDto participant, HttpServletRequest request) {
-        return participantProvider.update(modelMapper.map(participant, Participant.class));
+    public Participant update(@RequestBody ParticipantDto participantDto, HttpServletRequest request) {
+        final Club club;
+        if (participantDto.getClub() != null) {
+            club = clubProvider.get(participantDto.getClub().getId());
+        } else {
+            club = null;
+        }
+        final Participant participant = modelMapper.map(participantDto, Participant.class);
+        participant.setClub(club);
+        return participantProvider.update(modelMapper.map(participantDto, Participant.class));
     }
 }
