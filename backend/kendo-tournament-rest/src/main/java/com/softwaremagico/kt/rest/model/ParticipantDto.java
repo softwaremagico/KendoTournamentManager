7f54f70446de9718c3364c398563e1f0642420fc
Software Magico <softwaremagico@gmail.com>
2022-01-16 17:48:15 +0100

    Setting value club

--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/model/ParticipantDto.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/model/ParticipantDto.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.rest.model;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -33,6 +33,8 @@ public class ParticipantDto {
     private String name = "";
 
     private String lastname = "";
+
+    private ClubDto club;
 
     public Integer getId() {
         return id;
@@ -64,5 +66,13 @@ public class ParticipantDto {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public ClubDto getClub() {
+        return club;
+    }
+
+    public void setClub(ClubDto club) {
+        this.club = club;
     }
 }
