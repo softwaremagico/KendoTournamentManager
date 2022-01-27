de01d301f888eb4b9547e0705e4459829f058a19
Software Magico <softwaremagico@gmail.com>
2022-01-27 11:30:45 +0100

    Added brute force attack detection

--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/exceptions/UserBlockedException.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/exceptions/UserBlockedException.java
@@ -1,51 +1,39 @@
-package com.softwaremagico.kt.persistence.entities;
+package com.softwaremagico.kt.rest.exceptions;
 
 /*-
  * #%L
- * Kendo Tournament Manager (Persistence)
+ * Kendo Tournament Manager (Rest)
  * %%
  * Copyright (C) 2021 - 2022 Softwaremagico
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
+import com.softwaremagico.kt.logger.ExceptionType;
+import com.softwaremagico.kt.logger.LoggedHttpException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.persistence.*;
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UserBlockedException extends LoggedHttpException {
+    private static final long serialVersionUID = 7032994921678894370L;
 
-@Entity
-@Cacheable
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
-@Table(name = "duels")
-public class Duel {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "competitor1")
-    private Participant competitor1;
-
-    @ManyToOne
-    @JoinColumn(name = "competitor2")
-    private Participant competitor2;
-
-
+    public UserBlockedException(Class<?> clazz, String message) {
+        super(clazz, message, ExceptionType.INFO, HttpStatus.UNAUTHORIZED);
+    }
 }
---
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/security/AuthenticationSuccessEventListener.java
@@ -0,0 +1,57 @@
+package com.softwaremagico.kt.rest.security;
+
+/*-
+ * #%L
+ * Kendo Tournament Manager (Rest)
+ * %%
+ * Copyright (C) 2021 - 2022 Softwaremagico
+ * %%
+ * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
+ * <softwaremagico@gmail.com> Valencia (Spain).
+ *  
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AuthenticationSuccessEventListener implements
+        ApplicationListener<AuthenticationSuccessEvent> {
+
+    private final HttpServletRequest request;
+
+    private final BruteForceService bruteForceService;
+
+    @Autowired
+    public AuthenticationSuccessEventListener(HttpServletRequest request, BruteForceService bruteForceService) {
+        this.request = request;
+        this.bruteForceService = bruteForceService;
+    }
+
+    @Override
+    public void onApplicationEvent(final AuthenticationSuccessEvent e) {
+        final String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null) {
+            bruteForceService.loginSucceeded(request.getRemoteAddr());
+        } else {
+            bruteForceService.loginSucceeded(xfHeader.split(",")[0]);
+        }
+    }
+}
