3cfb452f59d2152481e65d66a511cc85bfeac1cb
Software Magico <softwaremagico@gmail.com>
2022-01-27 13:44:51 +0100

    Added test

--- backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/security/BruteForceTests.java
+++ backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/security/BruteForceTests.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.security;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -28,6 +28,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softwaremagico.kt.persistence.entities.AuthenticatedUser;
 import com.softwaremagico.kt.rest.controllers.AuthenticatedUserController;
+import com.softwaremagico.kt.rest.security.BruteForceService;
 import com.softwaremagico.kt.rest.security.dto.AuthRequest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -41,6 +42,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -69,6 +71,9 @@ public class BruteForceTests extends AbstractTestNGSpringContextTests {
     @Autowired
     private AuthenticatedUserController authenticatedUserController;
 
+    @Autowired
+    private BruteForceService bruteForceService;
+
     private <T> String toJson(T object) throws JsonProcessingException {
         return objectMapper.writeValueAsString(object);
     }
@@ -89,6 +94,7 @@ public class BruteForceTests extends AbstractTestNGSpringContextTests {
         authenticatedUserController.findAll().forEach(authenticatedUser -> {
             authenticatedUserController.delete(authenticatedUser);
         });
+        bruteForceService.reset();
     }
 
     @Test
@@ -133,5 +139,45 @@ public class BruteForceTests extends AbstractTestNGSpringContextTests {
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized())
                 .andExpect(MockMvcResultMatchers.header().doesNotExist(HttpHeaders.AUTHORIZATION))
                 .andReturn();
+    }
+
+    @Test
+    public void testLoginCountReset() throws Exception {
+        AuthenticatedUser authenticatedUser = authenticatedUserController.createUser(USER_NAME, USER_FULL_NAME, USER_PASSWORD);
+
+        AuthRequest request = new AuthRequest();
+        request.setUsername(authenticatedUser.getUsername());
+        request.setPassword("invalidPassword");
+
+        // 3 attempts to block the user.
+        this.mockMvc
+                .perform(post("/api/public/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.header().doesNotExist(HttpHeaders.AUTHORIZATION))
+                .andReturn();
+
+        this.mockMvc
+                .perform(post("/api/public/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.header().doesNotExist(HttpHeaders.AUTHORIZATION))
+                .andReturn();
+
+        Assert.assertEquals(bruteForceService.getElement("127.0.0.1"), Integer.valueOf(2));
+
+        //Correct password, attempts reset.
+        request.setPassword(USER_PASSWORD);
+        this.mockMvc
+                .perform(post("/api/public/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.AUTHORIZATION))
+                .andReturn();
+
+        Assert.assertNull(bruteForceService.getElement("127.0.0.1"));
     }
 }
