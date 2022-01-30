e7efb2d30c3b7cecfcbf5ce2d64e6ac0f08b6166
Software Magico <softwaremagico@gmail.com>
2022-01-30 19:24:26 +0100

    Added tournament basic rest services

---
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/exceptions/TournamentNotFoundException.java
@@ -0,0 +1,50 @@
+package com.softwaremagico.kt.core.exceptions;
+
+/*-
+ * #%L
+ * Kendo Tournament Manager (Core)
+ * %%
+ * Copyright (C) 2021 - 2022 Softwaremagico
+ * %%
+ * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
+ * <softwaremagico@gmail.com> Valencia (Spain).
+ *  
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.softwaremagico.kt.logger.ExceptionType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TournamentNotFoundException extends NotFoundException {
+    private static final long serialVersionUID = 3091553855925575861L;
+
+    public TournamentNotFoundException(Class<?> clazz, String message, ExceptionType type) {
+        super(clazz, message, type);
+    }
+
+    public TournamentNotFoundException(Class<?> clazz, String message) {
+        super(clazz, message, ExceptionType.WARNING);
+    }
+
+    public TournamentNotFoundException(Class<?> clazz) {
+        this(clazz, "User not found");
+    }
+
+    public TournamentNotFoundException(Class<?> clazz, Throwable e) {
+        super(clazz, e);
+    }
+}
