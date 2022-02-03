239f10ebaf1e50626a96bbbfa5d551c430f80dcb
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-02-03 16:31:01 +0100

    Updated translations

--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/model/TournamentDto.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/model/TournamentDto.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.rest.model;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -29,6 +29,8 @@ import com.softwaremagico.kt.persistence.values.TournamentType;
 
 public class TournamentDto {
 
+    private Integer id;
+
     private String name;
 
     private Integer shiaijos;
@@ -38,6 +40,14 @@ public class TournamentDto {
     private TournamentType type;
 
     private TournamentScore tournamentScore;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
