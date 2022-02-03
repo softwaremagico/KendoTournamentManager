a32a3f755f3e3e109b668465641e3ad1902f089b
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-02-03 17:05:34 +0100

    Adding Tournament Types

--- frontend/src/app/models/tournament.ts
+++ frontend/src/app/models/tournament.ts
@@ -1,7 +1,9 @@
+import {TournamentType} from "./TournamentType";
+
 export class Tournament {
   public id?: number;
   public name: string;
   public shiaijos?: number;
   public teamSize?: number;
-  public type?: string;
+  public type?: TournamentType;
 }
