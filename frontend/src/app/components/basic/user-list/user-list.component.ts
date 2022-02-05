4bb3139a48e0b4cb2f02ac915960c1ac71d12717
Software Magico <softwaremagico@gmail.com>
2022-02-05 09:36:47 +0100

    Added user filter

--- frontend/src/app/components/basic/user-list/user-list.component.ts
+++ frontend/src/app/components/basic/user-list/user-list.component.ts
@@ -10,10 +10,24 @@ export class UserListComponent implements OnInit {
 
   @Input()
   userListData: UserListData;
+  originalUsers: UserListData;
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  filter(event: Event) {
+    const filter = (event.target as HTMLInputElement).value.toLowerCase();
+    if (this.originalUsers === undefined) {
+      this.originalUsers = Object.assign([], this.userListData);
+    }
+    this.userListData = Object.assign([], this.originalUsers);
+    this.userListData.participants = this.userListData.participants.filter(user => user.lastname.toLowerCase().includes(filter) ||
+      user.name.toLowerCase().includes(filter) || user.idCard.toLowerCase().includes(filter));
   }
 
 }
