dbe448c8305c2f9189dfae3274af74df126aedb6
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-02-07 16:47:04 +0100

    Generalizing roles

--- frontend/src/app/models/RoleType.ts
+++ frontend/src/app/models/RoleType.ts
@@ -1,16 +1,22 @@
 export enum RoleType {
-  REFEREE = 'REFEREE',
   COMPETITOR = 'COMPETITOR',
+  REFEREE = 'REFEREE',
   VOLUNTEER = 'VOLUNTEER'
 }
 
 export namespace RoleType {
-  export function getByKey(key: string) {
+  export function getByKey(key: string): string | undefined {
     for (const valueKey in RoleType) {
       if ((RoleType as any)[valueKey] === key) {
         return valueKey;
       }
     }
     return undefined;
+  }
+}
+
+export namespace RoleType {
+  export function getKeys(): string[] {
+    return Object.keys(RoleType).filter(enumValue => !(typeof (RoleType[enumValue as RoleType]) === 'function'))
   }
 }
