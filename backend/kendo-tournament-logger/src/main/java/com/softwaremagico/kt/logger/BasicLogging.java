be6110df659ad7f05e7dff5fe7c766e7c250881a
softwaremagico <softwaremagico@gmail.com>
2022-02-22 12:30:52 +0100

    Skipping some actions before logging

--- backend/kendo-tournament-logger/src/main/java/com/softwaremagico/kt/logger/BasicLogging.java
+++ backend/kendo-tournament-logger/src/main/java/com/softwaremagico/kt/logger/BasicLogging.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.logger;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -79,13 +79,15 @@ public class BasicLogging extends AbstractLogging {
 
     @Around(value = "(selectAll() || isAnnotated()) && !avoidClasses()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        final StopWatch stopWatch = new StopWatch();
-        Object returnValue = null;
-        stopWatch.start();
-        returnValue = joinPoint.proceed();
-        stopWatch.stop();
-        log(stopWatch.getTotalTimeMillis(), joinPoint);
-        return returnValue;
+        if (logger.isDebugEnabled()) {
+            final StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            Object returnValue = joinPoint.proceed();
+            stopWatch.stop();
+            log(stopWatch.getTotalTimeMillis(), joinPoint);
+            return returnValue;
+        }
+        return null;
     }
 
     /**
