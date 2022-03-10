3f8fb6c623fee99723b6c9268d7802947ca5be9a
softwaremagico <softwaremagico@gmail.com>
2022-03-10 15:48:32 +0100

    Added more log information

--- backend/kendo-tournament-logger/src/main/java/com/softwaremagico/kt/logger/BasicLogger.java
+++ backend/kendo-tournament-logger/src/main/java/com/softwaremagico/kt/logger/BasicLogger.java
@@ -8,23 +8,27 @@ package com.softwaremagico.kt.logger;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
 import org.slf4j.Logger;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public abstract class BasicLogger {
 
@@ -145,8 +149,20 @@ public abstract class BasicLogger {
         severe(logger, className, messageTemplate, arguments);
     }
 
+    public static void errorMessageNotification(Logger logger, String messageTemplate, Object... arguments) {
+        severe(logger, messageTemplate, arguments);
+    }
+
     public static void errorMessageNotification(Logger logger, String className, Throwable throwable) {
         logger.error("Exception on class {}:\n", className, throwable);
+    }
+
+
+    public static String getStackTrace(Throwable throwable) {
+        final Writer writer = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(writer);
+        throwable.printStackTrace(printWriter);
+        return writer.toString();
     }
 
 }
