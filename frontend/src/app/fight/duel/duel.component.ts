a3ba5d9e00b868023ba8d06c078c2b6c7a2dae98
Software Magico <softwaremagico@gmail.com>
2022-04-17 08:25:04 +0200

    Adding score components

---
+++ frontend/src/app/fight/duel/duel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-duel',
+  templateUrl: './duel.component.html',
+  styleUrls: ['./duel.component.scss']
+})
+export class DuelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
