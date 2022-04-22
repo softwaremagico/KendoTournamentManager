7d4826a3e22b582910307519f7a71d51a63edf12
softwaremagico <softwaremagico@gmail.com>
2022-04-22 15:52:12 +0200

    Loading fights from tournament

--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/parsers/FightParser.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/parsers/FightParser.java
@@ -8,22 +8,24 @@ package com.softwaremagico.kt.rest.parsers;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
+import com.softwaremagico.kt.core.exceptions.TeamNotFoundException;
+import com.softwaremagico.kt.core.exceptions.TournamentNotFoundException;
 import com.softwaremagico.kt.core.providers.FightProvider;
 import com.softwaremagico.kt.core.providers.TeamProvider;
 import com.softwaremagico.kt.core.providers.TournamentProvider;
@@ -67,14 +69,14 @@ public class FightParser {
 
         final Tournament tournament = tournamentProvider.get(fightDto.getTournament().getId());
         if (tournament == null) {
-            throw new BadRequestException(getClass(), "Selected tournament is wrong");
+            throw new TournamentNotFoundException(getClass(), "Tournament not found with id '" + fightDto.getTournament().getId() + "'.");
         }
         fight.setTournament(tournament);
 
         if (fightDto.getTeam1().getId() != null) {
             final Team team1 = teamProvider.get(fightDto.getTeam1().getId());
             if (team1 == null) {
-                throw new BadRequestException(getClass(), "Selected team1 is wrong");
+                throw new TeamNotFoundException(getClass(), "Team1 not found with id '" + fightDto.getTeam1().getId() + "'.");
             }
             fight.setTeam1(team1);
         }
@@ -82,7 +84,7 @@ public class FightParser {
         if (fightDto.getTeam2().getId() != null) {
             final Team team2 = teamProvider.get(fightDto.getTeam2().getId());
             if (team2 == null) {
-                throw new BadRequestException(getClass(), "Selected team2 is wrong");
+                throw new TeamNotFoundException(getClass(), "Team2 not found with id '" + fightDto.getTeam2().getId() + "'.");
             }
             fight.setTeam2(team2);
         }
