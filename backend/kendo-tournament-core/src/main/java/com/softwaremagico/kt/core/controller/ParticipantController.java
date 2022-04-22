f0f0aca0f8774bb2a722bcf8af34eea2ede5abda
softwaremagico <softwaremagico@gmail.com>
2022-04-22 17:17:02 +0200

    Created ParticipantController

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/controller/ParticipantController.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/controller/ParticipantController.java
@@ -1,43 +1,27 @@
 package com.softwaremagico.kt.core.controller;
 
 import com.softwaremagico.kt.core.controller.models.ParticipantDTO;
-import com.softwaremagico.kt.core.converters.IElementConverter;
+import com.softwaremagico.kt.core.converters.ParticipantConverter;
 import com.softwaremagico.kt.core.converters.models.ParticipantConverterRequest;
 import com.softwaremagico.kt.core.providers.ParticipantProvider;
 import com.softwaremagico.kt.persistence.entities.Participant;
+import com.softwaremagico.kt.persistence.repositories.ParticipantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.util.Collection;
-
 @Controller
-public class ParticipantController implements IElementConverter<Participant, ParticipantDTO, ParticipantConverterRequest> {
-    private final ParticipantProvider participantProvider;
+public class ParticipantController extends BasicInsertableController<Participant, ParticipantDTO, ParticipantRepository,
+        ParticipantProvider, ParticipantConverterRequest, ParticipantConverter> {
 
 
     @Autowired
-    public ParticipantController(ParticipantProvider participantProvider) {
-        this.participantProvider = participantProvider;
-    }
-
-
-    @Override
-    public ParticipantDTO convert(ParticipantConverterRequest from) {
-        return null;
+    public ParticipantController(ParticipantProvider provider, ParticipantConverter converter) {
+        super(provider, converter);
     }
 
     @Override
-    public Collection<ParticipantDTO> convertAll(Collection<ParticipantConverterRequest> from) {
-        return null;
+    protected ParticipantConverterRequest createConverterRequest(Participant participant) {
+        return new ParticipantConverterRequest(participant);
     }
 
-    @Override
-    public ParticipantConverterRequest reverse(ParticipantDTO to) {
-        return null;
-    }
-
-    @Override
-    public Collection<ParticipantConverterRequest> reverseAll(Collection<ParticipantDTO> to) {
-        return null;
-    }
 }
