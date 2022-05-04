c8c648e4702aa1e34c1e62cfcc6fd856438de894
Software Magico <softwaremagico@gmail.com>
2022-05-04 18:19:19 +0100

    Two faults are a hansoku

--- frontend/src/app/models/duel.ts
+++ frontend/src/app/models/duel.ts
@@ -11,4 +11,27 @@ export class Duel extends Element {
   public competitor1Score: Score[];
   public competitor2Score: Score[];
   public type: TournamentType;
+
+  public static override copy(source: Duel, target: Duel): void {
+    Element.copy(source, target);
+    target.competitor1Fault = source.competitor1Fault;
+    target.competitor2Fault = source.competitor2Fault;
+    target.type = source.type;
+    if (source.competitor1 !== undefined) {
+      target.competitor1 = Participant.clone(source.competitor1);
+    }
+    if (source.competitor2 !== undefined) {
+      target.competitor2 = Participant.clone(source.competitor2);
+    }
+    target.competitor1Score = [];
+    target.competitor2Score = [];
+    source.competitor1Score.forEach(score => target.competitor1Score.push(score));
+    source.competitor2Score.forEach(score => target.competitor2Score.push(score));
+  }
+
+  public static clone(data: Duel): Duel {
+    const instance: Duel = new Duel();
+    this.copy(data, instance);
+    return instance;
+  }
 }
