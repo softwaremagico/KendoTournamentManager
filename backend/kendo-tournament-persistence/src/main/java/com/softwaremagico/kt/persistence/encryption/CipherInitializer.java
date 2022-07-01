17e39fe90afb9aeca7367ed552af0ff1e6ad7589
Software Magico <softwaremagico@gmail.com>
2022-07-01 17:09:49 +0200

    Changing methods to private

--- backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/encryption/CipherInitializer.java
+++ backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/encryption/CipherInitializer.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.persistence.encryption;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -29,10 +29,7 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.Charset;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.security.spec.AlgorithmParameterSpec;
 
 public class CipherInitializer {
@@ -50,12 +47,12 @@ public class CipherInitializer {
 		return cipher;
 	}
 
-	void callCipherInit(Cipher cipher, int encryptionMode, Key secretKey, AlgorithmParameterSpec algorithmParameters) throws InvalidKeyException,
+	private void callCipherInit(Cipher cipher, int encryptionMode, Key secretKey, AlgorithmParameterSpec algorithmParameters) throws InvalidKeyException,
 			InvalidAlgorithmParameterException {
 		cipher.init(encryptionMode, secretKey, algorithmParameters);
 	}
 
-	int getCipherBlockSize(Cipher cipher) {
+	private int getCipherBlockSize(Cipher cipher) {
 		return cipher.getBlockSize();
 	}
 
