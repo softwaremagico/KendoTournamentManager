4abd6ebbc8052a75a5d6b68c4701f0523a218aad
softwaremagico <softwaremagico@gmail.com>
2022-08-06 15:50:07 +0200

    Selecting fights and duels effect

--- frontend/src/app/components/fight/fight.component.ts
+++ frontend/src/app/components/fight/fight.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Fight} from "../../models/fight";
+import {Duel} from "../../models/duel";
 
 @Component({
   selector: 'fight',
@@ -11,6 +12,11 @@ export class FightComponent implements OnInit {
   @Input()
   fight: Fight;
 
+  @Input()
+  selected: boolean;
+
+  selectedDuel: Duel | undefined;
+
   ngOnInit(): void {
     // This is intentional
   }
@@ -20,6 +26,10 @@ export class FightComponent implements OnInit {
       return this.fight.tournament.teamSize > 1;
     }
     return true;
+  }
+
+  selectDuel(duel: Duel) {
+    this.selectedDuel = duel;
   }
 
 }
