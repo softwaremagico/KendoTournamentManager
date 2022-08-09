98e73db08fb1ac167968305f95be0017d9e09089
softwaremagico <softwaremagico@gmail.com>
2022-08-09 09:12:04 +0200

    Adding services to update groups.

--- frontend/src/app/models/fight.ts
+++ frontend/src/app/models/fight.ts
@@ -13,4 +13,32 @@ export class Fight extends Element {
   public duels: Duel[];
 
 
+  public static override copy(source: Fight, target: Fight): void {
+    Element.copy(source, target);
+    if (source.tournament !== undefined) {
+      target.tournament = Tournament.clone(source.tournament);
+    }
+    target.shiaijo = source.shiaijo;
+    target.level = source.level;
+    target.finishedAt = source.finishedAt;
+    if (source.team1 !== undefined) {
+      target.team1 = Team.clone(source.team1);
+    }
+    if (source.team2 !== undefined) {
+      target.team2 = Team.clone(source.team2);
+    }
+    if (source.duels !== undefined) {
+      target.duels = [];
+      for (let duel of target.duels) {
+        target.duels.push(Duel.clone(duel));
+      }
+    }
+  }
+
+  public static clone(data: Fight): Fight {
+    const instance: Fight = new Fight();
+    this.copy(data, instance);
+    return instance;
+  }
+
 }
