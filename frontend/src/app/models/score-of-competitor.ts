c836f57e9782552bc5e5cf853662d0c745ddcd83
softwaremagico <softwaremagico@gmail.com>
2022-08-11 21:05:12 +0200

    Showing competitors ranking

--- frontend/src/app/models/score-of-competitor.ts
+++ frontend/src/app/models/score-of-competitor.ts
@@ -6,16 +6,16 @@ export class ScoreOfCompetitor {
   public drawDuels: number;
   public hits: number;
   public duelsDone: number;
-  public fightsWon: number;
-  public fightsDraw: number;
+  public wonFights: number;
+  public drawFights: number;
 
   public static copy(source: ScoreOfCompetitor, target: ScoreOfCompetitor): void {
     target.wonDuels = source.wonDuels;
     target.drawDuels = source.drawDuels;
     target.hits = source.hits;
     target.duelsDone = source.duelsDone;
-    target.fightsWon = source.fightsWon;
-    target.fightsDraw = source.fightsDraw;
+    target.wonFights = source.wonFights;
+    target.drawFights = source.drawFights;
     if (source.competitor !== undefined) {
       target.competitor = Participant.clone(source.competitor);
     }
