219e450fdd144ad7a41207be36bcbb1b7a8a4187
softwaremagico <softwaremagico@gmail.com>
2022-08-12 19:04:02 +0200

    Creating PDF for competitors list

---
+++ backend/kendo-tournament-pdf/src/test/java/com/softwaremagico/kt/TestListener.java
@@ -0,0 +1,70 @@
+package com.softwaremagico.kt;
+
+/*-
+ * #%L
+ * Kendo Tournament Manager (Persistence)
+ * %%
+ * Copyright (C) 2021 - 2022 Softwaremagico
+ * %%
+ * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
+ * <softwaremagico@gmail.com> Valencia (Spain).
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.softwaremagico.kt.logger.TestLogging;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        TestLogging.info("### Test started '" + result.getMethod().getMethodName() + "' from '" + result.getTestClass().getName() + "'.");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        TestLogging.info("### Test finished '" + result.getMethod().getMethodName() + "' from '" + result.getTestClass().getName() + "' (" +
+                (result.getEndMillis() - result.getStartMillis()) + "ms).");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        TestLogging.errorMessage(this.getClass().getName(), "### Test failed '" + result.getMethod().getMethodName() + "' from '" + result.getTestClass().getName() +
+                "' (" + (result.getEndMillis() - result.getStartMillis()) + "ms).");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        TestLogging.info(this.getClass().getName(), "##### Starting tests from '" + context.getName() + "'.");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        TestLogging.info(this.getClass().getName(), "##### Tests finished from '" + context.getName() + "'.");
+    }
+}
--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/services/RankingServices.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/rest/services/RankingServices.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.rest.services;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -30,6 +30,7 @@ import com.softwaremagico.kt.core.score.ScoreOfTeam;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,6 +70,16 @@ public class RankingServices {
     }
 
     @PreAuthorize("hasRole('ROLE_VIEWER')")
+    @Operation(summary = "Gets participants' ranking in a pdf file.", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping(value = "/competitors/tournament/{tournamentId}/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public InputStreamResource getCompetitorsScoreRankingTournamentAsPdf(@Parameter(description = "Id of an existing tournament", required = true)
+                                                                         @PathVariable("tournamentId") Integer tournamentId,
+                                                                         HttpServletRequest request) {
+        List<ScoreOfCompetitor> scores = rankingController.getCompetitorsScoreRankingFromTournament(tournamentId);
+        return null;
+    }
+
+    @PreAuthorize("hasRole('ROLE_VIEWER')")
     @Operation(summary = "Gets teams' ranking.", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping(value = "/teams/group/{groupId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ScoreOfTeam> getTeamsScoreRankingFromGroup(@Parameter(description = "Id of an existing group", required = true)
@@ -84,5 +95,15 @@ public class RankingServices {
                                                                 @PathVariable("tournamentId") Integer tournamentId,
                                                                 HttpServletRequest request) {
         return rankingController.getTeamsScoreRankingFromTournament(tournamentId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_VIEWER')")
+    @Operation(summary = "Gets teams' ranking in a pdf file.", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping(value = "/teams/tournament/{tournamentId}/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public InputStreamResource getTeamsScoreRankingFromTournamentAsPdf(@Parameter(description = "Id of an existing tournament", required = true)
+                                                                       @PathVariable("tournamentId") Integer tournamentId,
+                                                                       HttpServletRequest request) {
+        List<ScoreOfTeam> scores = rankingController.getTeamsScoreRankingFromTournament(tournamentId);
+        return null;
     }
 }
