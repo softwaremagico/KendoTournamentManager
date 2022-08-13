ac2e9849574fa383fa993a9a154b697f19e9f9b8
softwaremagico <softwaremagico@gmail.com>
2022-08-13 15:21:44 +0200

    Correctly download pdf from swagger

--- backend/kendo-tournament-pdf/src/main/java/com/softwaremagico/kt/pdf/ParentList.java
+++ backend/kendo-tournament-pdf/src/main/java/com/softwaremagico/kt/pdf/ParentList.java
@@ -1,5 +1,29 @@
 package com.softwaremagico.kt.pdf;
 
+/*-
+ * #%L
+ * Kendo Tournament Manager (PDF)
+ * %%
+ * Copyright (C) 2021 - 2022 Softwaremagico
+ * %%
+ * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
+ * <softwaremagico@gmail.com> Valencia (Spain).
+ *  
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import com.lowagie.text.Document;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
@@ -12,12 +36,10 @@ import com.lowagie.text.pdf.PdfWriter;
 import java.awt.*;
 
 public abstract class ParentList extends PdfDocument {
-    protected final static int FOOTER_BORDER = 0;
-    protected final static int HEADER_BORDER = 0;
-
-    protected final static int CELL_BORDER = 0;
-
-    private final static float HEADER_SEPARATOR = 20f;
+    protected static final int FOOTER_BORDER = 0;
+    protected static final int HEADER_BORDER = 0;
+    protected static final int CELL_BORDER = 0;
+    private static final float HEADER_SEPARATOR = 20f;
 
 
     /**
@@ -34,7 +56,7 @@ public abstract class ParentList extends PdfDocument {
     protected abstract void createHeaderRow(Document document, PdfPTable mainTable, float width, float height, PdfWriter writer, BaseFont font, int fontSize);
 
     protected void createRowSeparator(PdfPTable mainTable) {
-        PdfPCell cell = new PdfPCell();
+        final PdfPCell cell = new PdfPCell();
         cell.setFixedHeight(HEADER_SEPARATOR);
         cell.setBorder(0);
         cell.setColspan(getTableWidths().length);
