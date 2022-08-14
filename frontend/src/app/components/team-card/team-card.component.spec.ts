a91ae36f9991287cead77c2b9bc8762e42de78dd
softwaremagico <softwaremagico@gmail.com>
2022-08-14 08:25:03 +0200

    Download correctly PDFs in Angular

--- frontend/src/app/components/team-card/team-card.component.spec.ts
+++ frontend/src/app/components/team-card/team-card.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TeamCardComponent } from './team-card.component';
+import {TeamCardComponent} from './team-card.component';
 
 describe('TeamCardComponent', () => {
   let component: TeamCardComponent;
