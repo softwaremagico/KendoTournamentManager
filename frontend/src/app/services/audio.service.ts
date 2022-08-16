38098a1cf0244fae0d04b15cf8fe20a1c9918cda
softwaremagico <softwaremagico@gmail.com>
2022-08-16 09:33:25 +0200

    Added alarm to timer

---
+++ frontend/src/app/services/audio.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AudioService {
+
+  constructor() { }
+
+  playAlarm(): void {
+    const audio = new Audio();
+    audio.src = '../../assets/audio/alarm.wav';
+    audio.load();
+    audio.play();
+  }
+}
