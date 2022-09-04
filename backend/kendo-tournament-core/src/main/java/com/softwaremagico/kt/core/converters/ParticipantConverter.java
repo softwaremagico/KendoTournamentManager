6f104a9925a952f972b2f6ef48009d20bdf9ce40
Software Magico <softwaremagico@gmail.com>
2022-09-04 15:33:32 +0100

    Trying map

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/converters/ParticipantConverter.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/converters/ParticipantConverter.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.core.converters;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -44,6 +44,9 @@ public class ParticipantConverter extends ElementConverter<Participant, Particip
 
     @Override
     public ParticipantDTO convert(ParticipantConverterRequest from) {
+        if (from == null) {
+            return null;
+        }
         final ParticipantDTO participantDTO = new ParticipantDTO();
         BeanUtils.copyProperties(from.getEntity(), participantDTO, ConverterUtils.getNullPropertyNames(from.getEntity()));
         participantDTO.setClub(clubConverter.convert(new ClubConverterRequest(from.getEntity().getClub())));
