65e9b0aa32addc622243e046349c5b9cde20d452
Software Magico <softwaremagico@gmail.com>
2022-09-12 20:11:52 +0100

    Avoiding invalid scores when team member is null.

--- frontend/src/app/models/score.ts
+++ frontend/src/app/models/score.ts
@@ -34,6 +34,23 @@ export namespace Score {
 }
 
 export namespace Score {
+  export function noCompetitor(): Score[] {
+    const scores: Score[] = [];
+    scores.push(Score.IPPON);
+    scores.push(Score.EMPTY);
+    return scores;
+  }
+}
+
+export namespace Score {
+  export function clear(): Score[] {
+    const scores: Score[] = [];
+    scores.push(Score.EMPTY);
+    return scores;
+  }
+}
+
+export namespace Score {
   export function getEnumKeyByEnumValue<T extends { [index: string]: string }>(myEnum: T, enumValue: string): keyof T | null {
     let keys = Object.keys(myEnum).filter(x => myEnum[x] == enumValue);
     return keys.length > 0 ? keys[0] : null;
