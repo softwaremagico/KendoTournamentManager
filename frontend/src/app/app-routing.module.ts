9b167f98dd87c1c6948f01db86727e0a71e9cb88
Software Magico <softwaremagico@gmail.com>
2022-09-22 18:36:15 +0100

    Created User view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import {LoginComponent} from './views/login/login.component';
 import {ParticipantListComponent} from "./views/participant-list/participant-list.component";
 import {TournamentListComponent} from "./views/tournament-list/tournament-list.component";
 import {FightListComponent} from "./views/fight-list/fight-list.component";
+import {AuthenticatedUserListComponent} from "./views/authenticated-user-list/authenticated-user-list.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/tournaments', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
   {path: 'clubs', component: ClubListComponent, canActivate: [LoggedInService]},
+  {path: 'users', component: AuthenticatedUserListComponent, canActivate: [LoggedInService]},
   {path: 'participants', component: ParticipantListComponent, canActivate: [LoggedInService]},
   {path: 'tournaments', component: TournamentListComponent, canActivate: [LoggedInService]},
   {path: 'tournaments/fights', component: FightListComponent, canActivate: [LoggedInService]}
