75e792257df3b0514d3df4701ec70d5482aa04a6
Software Magico <softwaremagico@gmail.com>
2022-10-23 11:35:01 +0100

    Basic RBAC service

--- frontend/src/app/views/login/login.component.ts
+++ frontend/src/app/views/login/login.component.ts
@@ -5,8 +5,9 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MessageService} from "../../services/message.service";
 import {LoggerService} from "../../services/logger.service";
+import {RbacService} from "../../services/rbac.service";
 
-const { version: appVersion } = require('../../../../package.json')
+const {version: appVersion} = require('../../../../package.json')
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,7 @@ export class LoginComponent {
   loginForm: FormGroup;
   appVersion: string;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, public loginService: LoginService,
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private loginService: LoginService, private rbacService: RbacService,
               private formBuilder: FormBuilder, private messageService: MessageService, private loggerService: LoggerService) {
     this.appVersion = appVersion;
     this.loginForm = this.formBuilder.group({
@@ -29,15 +30,16 @@ export class LoginComponent {
   }
 
   login() {
-    this.loginService.login(this.loginForm.controls['username'].value, this.loginForm.controls['password'].value).subscribe(data => {
-        this.loginService.setJwtValue(data.jwt);
-
+    this.loginService.login(this.loginForm.controls['username'].value, this.loginForm.controls['password'].value).subscribe({
+      next: (authenticatedUser) => {
+        this.loginService.setJwtValue(authenticatedUser.jwt);
+        this.rbacService.setRoles(authenticatedUser.roles);
         let returnUrl = this.activatedRoute.snapshot.queryParams["returnUrl"];
         this.router.navigate([returnUrl]);
         this.messageService.infoMessage("userloggedInMessage");
         localStorage.setItem('username', (this.loginForm.controls['username'].value));
       },
-      error => {
+      error: (error) => {
         if (error.status === 401) {
           this.loggerService.info(`Error logging: ` + error);
           this.messageService.errorMessage("deniedUserError");
@@ -45,6 +47,7 @@ export class LoginComponent {
           console.error(error);
           this.messageService.errorMessage("backendError");
         }
-      });
+      }
+    });
   }
 }
