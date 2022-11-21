faf629087bd754a942dfb4b45d5b0ac1159e4cc3
Software Magico <softwaremagico@gmail.com>
2022-11-21 17:43:59 +0100

    Adding fights to fight panel

--- frontend/src/app/views/fight-list/fight-dialog-box/fight-dialog-box.component.ts
+++ frontend/src/app/views/fight-list/fight-dialog-box/fight-dialog-box.component.ts
@@ -7,6 +7,11 @@ import {TeamService} from "../../../services/team.service";
 import {Tournament} from "../../../models/tournament";
 import {CdkDrag, CdkDragDrop, CdkDropList, transferArrayItem} from "@angular/cdk/drag-drop";
 import {Team} from "../../../models/team";
+import {GroupService} from "../../../services/group.service";
+import {Group} from "../../../models/group";
+import {MessageService} from "../../../services/message.service";
+import {FightService} from "../../../services/fight.service";
+import {GroupUpdatedService} from "../../../services/notifications/group-updated.service";
 
 @Component({
   selector: 'app-fight-dialog-box',
@@ -17,7 +22,9 @@ export class FightDialogBoxComponent implements OnInit {
 
   teamListData: TeamListData = new TeamListData();
   tournament: Tournament;
+  previousFight: Fight | undefined;
   fight: Fight;
+  group: Group;
   title: string;
   action: Action;
   actionName: string;
@@ -31,11 +38,17 @@ export class FightDialogBoxComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<FightDialogBoxComponent>,
     private teamService: TeamService,
+    private fightService: FightService,
+    private groupServices: GroupService,
+    private messageService: MessageService,
+    private groupUpdatedService: GroupUpdatedService,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: {
-      title: string, action: Action, entity: Fight, tournament: Tournament,
+      title: string, action: Action, entity: Fight, group: Group, previousFight: Fight | undefined, tournament: Tournament,
       swappedColors: boolean, swappedTeams: boolean
     }
   ) {
+    this.group = data.group;
+    this.previousFight = data.previousFight;
     this.fight = data.entity;
     this.title = data.title;
     this.action = data.action;
@@ -75,6 +88,31 @@ export class FightDialogBoxComponent implements OnInit {
 
   checkDroppedElement(item: CdkDrag<Team>, drop: CdkDropList) {
     return (drop.data.length === 0 || drop.data.length === 1 && drop.data!.includes(item.data));
+  }
+
+  addFights() {
+    this.fight.team1 = this.selectedTeam1[0];
+    this.fight.team2 = this.selectedTeam2[0];
+    this.fight.tournament = this.group.tournament;
+    this.fight.shiaijo = 0;
+    this.fight.level = this.group.level;
+    this.fight.duels = [];
+
+    this.fightService.generateDuels(this.fight).subscribe(_fight => {
+      if (this.previousFight !== undefined) {
+        this.group.fights.splice(this.group.fights.indexOf(this.previousFight) + 1, 0, _fight);
+      } else if (!this.group.fights.includes(_fight)) {
+        this.group.fights.push(_fight);
+      }
+
+      this.groupServices.update(this.group).subscribe(_group => {
+        this.messageService.infoMessage("addFightMessage");
+        this.groupUpdatedService.isGroupUpdated.next(_group);
+        this.dialogRef.close();
+      });
+    });
+
+
   }
 
 }
