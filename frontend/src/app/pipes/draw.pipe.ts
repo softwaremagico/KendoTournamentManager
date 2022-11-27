8e74d4863311a3051218ecdbee17052eac2f776d
Software Magico <softwaremagico@gmail.com>
2022-11-27 19:20:17 +0100

    Draw calculus moved to pipe.

---
+++ frontend/src/app/pipes/draw.pipe.ts
@@ -0,0 +1,27 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Duel} from "../models/duel";
+import {Score} from "../models/score";
+
+@Pipe({
+  name: 'draw'
+})
+export class DrawPipe implements PipeTransform {
+
+  transform(duel: Duel, ...args: unknown[]): boolean {
+    if (!duel.duration) {
+      return false;
+    }
+    return duel.duration > 0 && this.countPoints(duel.competitor1Score) === this.countPoints(duel.competitor2Score);
+  }
+
+  countPoints(scores: Score[]): number {
+    let sum = 0;
+    scores.forEach(score => {
+      if (score != Score.EMPTY) {
+        sum++;
+      }
+    });
+    return sum;
+  }
+
+}
