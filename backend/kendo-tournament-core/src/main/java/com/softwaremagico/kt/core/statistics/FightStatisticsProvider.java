29de66f4b33a5c0f4412c579c93bbea2adfd5849
Software Magico <softwaremagico@gmail.com>
2022-12-06 17:03:52 +0100

    Added statistics tests

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/statistics/FightStatisticsProvider.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/statistics/FightStatisticsProvider.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.core.statistics;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -45,15 +45,15 @@ public class FightStatisticsProvider {
     }
 
 
-    public FightStatisticsDTO calculate(TournamentType tournamentType, boolean maximizeFights, Collection<TeamDTO> teams) {
+    public FightStatisticsDTO calculate(TournamentType tournamentType, boolean maximizeFights, int teamSize, Collection<TeamDTO> teams) {
         if (tournamentType == null || teams == null || teams.size() < 2) {
             return null;
         }
         switch (tournamentType) {
             case LEAGUE:
-                return calculateLeagueStatistics(teams);
+                return calculateLeagueStatistics(teamSize, teams);
             case LOOP:
-                return calculateLoopStatistics(maximizeFights, teams);
+                return calculateLoopStatistics(maximizeFights, teamSize, teams);
             case CUSTOMIZED:
             case KING_OF_THE_MOUNTAIN:
                 return null;
@@ -61,19 +61,20 @@ public class FightStatisticsProvider {
         return null;
     }
 
-    private FightStatisticsDTO calculateLeagueStatistics(Collection<TeamDTO> teams) {
+    private FightStatisticsDTO calculateLeagueStatistics(int teamSize, Collection<TeamDTO> teams) {
         final FightStatisticsDTO fightStatisticsDTO = new FightStatisticsDTO();
         fightStatisticsDTO.setFightNumber((teams.size() * (teams.size() - 1)) / 2);
         fightStatisticsDTO.setFightsByTeam((teams.size() - 1));
-        fightStatisticsDTO.setDuelNumber(getDuels(fightStatisticsDTO.getFightsByTeam(), teams));
-        fightStatisticsDTO.setTime(fightStatisticsDTO.getDuelNumber() * (duelProvider.getDurationAverage() + TIME_BETWEEN_DUELS) +
-                (long) TIME_BETWEEN_FIGHTS * fightStatisticsDTO.getFightNumber());
+        fightStatisticsDTO.setDuelNumber(getDuels(fightStatisticsDTO.getFightsByTeam(), teamSize, teams));
+        if (duelProvider.getDurationAverage() != null) {
+            fightStatisticsDTO.setTime(fightStatisticsDTO.getDuelNumber() * (duelProvider.getDurationAverage() + TIME_BETWEEN_DUELS) +
+                    (long) TIME_BETWEEN_FIGHTS * fightStatisticsDTO.getFightNumber());
+        }
         return fightStatisticsDTO;
     }
 
-    private FightStatisticsDTO calculateLoopStatistics(boolean maximizeFights, Collection<TeamDTO> teams) {
+    private FightStatisticsDTO calculateLoopStatistics(boolean maximizeFights, int teamSize, Collection<TeamDTO> teams) {
         final FightStatisticsDTO fightStatisticsDTO = new FightStatisticsDTO();
-        fightStatisticsDTO.setFightNumber((teams.size() * teams.size() - 1) / 2);
         if (maximizeFights) {
             fightStatisticsDTO.setFightNumber((teams.size() * (teams.size() - 1)));
             fightStatisticsDTO.setFightsByTeam((teams.size() - 1) * 2);
@@ -81,20 +82,23 @@ public class FightStatisticsProvider {
             fightStatisticsDTO.setFightNumber((teams.size() * teams.size() - 1) / 2);
             fightStatisticsDTO.setFightsByTeam((teams.size() - 1));
         }
-        fightStatisticsDTO.setDuelNumber(getDuels(fightStatisticsDTO.getFightsByTeam(), teams));
-        fightStatisticsDTO.setTime(fightStatisticsDTO.getDuelNumber() * duelProvider.getDurationAverage());
+        fightStatisticsDTO.setDuelNumber(getDuels(fightStatisticsDTO.getFightsByTeam(), teamSize, teams));
+        if (duelProvider.getDurationAverage() != null) {
+            fightStatisticsDTO.setTime(fightStatisticsDTO.getDuelNumber() * duelProvider.getDurationAverage());
+        }
         return fightStatisticsDTO;
     }
 
-    private int getDuels(int fightByTeam, Collection<TeamDTO> teams) {
+    private int getDuels(int fightByTeam, int teamSize, Collection<TeamDTO> teams) {
         final AtomicInteger counter = new AtomicInteger();
         final AtomicInteger missingMembers = new AtomicInteger();
 
         teams.forEach(teamDTO -> {
-            counter.addAndGet((teamDTO.getMembers().size() * fightByTeam) + missingMembers.get());
-            missingMembers.getAndIncrement();
+            counter.addAndGet((teamDTO.getMembers().size() * fightByTeam) - missingMembers.get());
+            missingMembers.addAndGet(teamSize - teamDTO.getMembers().size());
         });
 
-        return counter.get();
+        //Duels are counted twice, once for each team
+        return counter.get() / 2;
     }
 }
