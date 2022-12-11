8cba3df009240dc28093ebb585d68289bb8ab349
Software Magico <softwaremagico@gmail.com>
2022-12-11 16:17:14 +0100

    Fixed issue when auto generating teams

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/statistics/models/FightStatisticsDTO.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/statistics/models/FightStatisticsDTO.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.core.statistics.models;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -36,7 +36,11 @@ public class FightStatisticsDTO {
     }
 
     public void setFightsNumber(Integer fightsNumber) {
-        this.fightsNumber = fightsNumber;
+        if (fightsNumber != null && fightsNumber >= 0) {
+            this.fightsNumber = fightsNumber;
+        } else {
+            this.fightsNumber = null;
+        }
     }
 
     public Integer getDuelsNumber() {
@@ -44,7 +48,11 @@ public class FightStatisticsDTO {
     }
 
     public void setDuelsNumber(Integer duelsNumber) {
-        this.duelsNumber = duelsNumber;
+        if (duelsNumber != null && duelsNumber >= 0) {
+            this.duelsNumber = duelsNumber;
+        } else {
+            this.duelsNumber = null;
+        }
     }
 
     public Long getTime() {
