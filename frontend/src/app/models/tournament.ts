2faff055bc53e8e10f270daf3f749406d78398e8
Software Magico <softwaremagico@gmail.com>
2022-12-15 23:19:47 +0100

    Adding the possibility to disable tournaments to avoid further modifications.

--- frontend/src/app/models/tournament.ts
+++ frontend/src/app/models/tournament.ts
@@ -14,6 +14,7 @@ export class Tournament extends Element {
   public type?: TournamentType;
   public duelsDuration: number;
   public tournamentScore: TournamentScore;
+  public locked: boolean;
 
   constructor() {
     super();
@@ -28,6 +29,7 @@ export class Tournament extends Element {
     target.type = source.type;
     target.duelsDuration = source.duelsDuration;
     target.tournamentScore = source.tournamentScore;
+    target.locked = source.locked;
   }
 
   public static clone(data: Tournament): Tournament {
