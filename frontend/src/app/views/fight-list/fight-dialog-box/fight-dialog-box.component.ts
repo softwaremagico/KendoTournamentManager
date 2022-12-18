dabc6636dbadd8221ea845e7ca45b865d31f4833
Software Magico <softwaremagico@gmail.com>
2022-12-18 17:14:11 +0100

    Removing hardcoded group index

--- frontend/src/app/views/fight-list/fight-dialog-box/fight-dialog-box.component.ts
+++ frontend/src/app/views/fight-list/fight-dialog-box/fight-dialog-box.component.ts
@@ -95,12 +95,7 @@ export class FightDialogBoxComponent implements OnInit {
     this.fight.team2 = this.selectedTeam2[0];
 
     this.fightService.generateDuels(this.fight).subscribe(_fight => {
-      console.log('***REMOVED***->', this.previousFight)
-      console.log('2***REMOVED***->', _fight)
-      console.log('3***REMOVED***->', this.group.fights)
       if (this.previousFight !== undefined) {
-        console.log('### ', this.group.fights.findIndex(fight => this.previousFight?.id === fight.id
-        ) + 1)
         this.group.fights.splice(this.group.fights.findIndex(fight => this.previousFight?.id === fight.id
         ) + 1, 0, _fight);
       } else if (!this.group.fights.includes(_fight)) {
