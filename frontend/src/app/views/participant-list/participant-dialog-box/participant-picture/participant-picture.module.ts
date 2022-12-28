5dc0d967ead23491dc9f5920358e9cc3d8d17875
Software Magico <softwaremagico@gmail.com>
2022-12-28 10:50:58 +0100

    Adding camara gallery

--- frontend/src/app/views/participant-list/participant-dialog-box/participant-picture/participant-picture.module.ts
+++ frontend/src/app/views/participant-list/participant-dialog-box/participant-picture/participant-picture.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ParticipantPictureComponent} from "./participant-picture.component";
+import {ParticipantPictureDialogBoxComponent} from "./participant-picture-dialog-box.component";
 import {WebcamModule} from "ngx-webcam";
+import {RbacModule} from "../../../../pipes/rbac-pipe/rbac.module";
+import {MatIconModule} from "@angular/material/icon";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
 @NgModule({
-  declarations: [ParticipantPictureComponent],
+  declarations: [ParticipantPictureDialogBoxComponent],
   imports: [
     CommonModule,
-    WebcamModule
+    WebcamModule,
+    RbacModule,
+    MatIconModule,
+    TranslateModule,
+    MatDialogModule,
+    MatButtonModule
   ]
 })
 export class ParticipantPictureModule { }
