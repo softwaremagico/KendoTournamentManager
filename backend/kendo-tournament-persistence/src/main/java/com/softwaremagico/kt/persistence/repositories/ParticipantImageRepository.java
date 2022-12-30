562214923b1832e68b9a694ca828b7d260a4e32b
Software Magico <softwaremagico@gmail.com>
2022-12-30 17:12:24 +0100

    Fixing storing images

--- backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/repositories/ParticipantImageRepository.java
+++ backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/repositories/ParticipantImageRepository.java
@@ -38,4 +38,6 @@ public interface ParticipantImageRepository extends JpaRepository<ParticipantIma
 
     Optional<ParticipantImage> findByParticipant(Participant participant);
 
+    int deleteByParticipant(Participant participant);
+
 }
