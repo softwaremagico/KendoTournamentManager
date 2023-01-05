9debef8263eceec261cd46a1aec1aa866da67e5c
Software Magico <softwaremagico@gmail.com>
2023-01-05 15:56:52 +0100

    Setting random color to participant initials avatar

--- frontend/src/app/components/participant-picture/participant-picture.component.ts
+++ frontend/src/app/components/participant-picture/participant-picture.component.ts
@@ -30,4 +30,24 @@ export class ParticipantPictureComponent implements OnInit {
     this.participantInitials = this.nameUtils.getInitials(this.participant);
   }
 
+  public get circleStyle(): string {
+    return "background-color: " + this.getParticipantColor(this.participant);
+  }
+
+  getParticipantColor(participant: Participant): string {
+    let color = 'rgb(';
+    if (participant.id) {
+      const minR = participant.id % 2 == 0 ? 200 : 128;
+      const minG = participant.id % 3 == 0 ? 128 : 200;
+      const minB = participant.id % 2 == 1 ? 200 : 128;
+      color += ((Math.abs(participant.id)) % minR + (256 - minR)) + ", ";
+      color += ((Math.abs((participant.id * 31) + participant.id * (participant.id % 2 == 0 ? 1 : -1)) % minG + (256 - minG))) + ", ";
+      color += ((Math.abs(participant.id) + participant.id * (participant.id % 2 == 0 ? -1 : 1)) % minB + (256 - minB));
+    } else {
+      color += "255, 255, 255";
+    }
+    color += ');';
+    return color;
+  }
+
 }
