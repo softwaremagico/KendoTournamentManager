883bbbd3afbaf035b22baa0327f576c7f69b0c95
Software Magico <softwaremagico@gmail.com>
2023-01-08 12:25:28 +0100

    Added pop-up to show the participant picture

---
+++ frontend/src/app/components/participant-picture/picture-dialog-box/picture-dialog-box.component.ts
@@ -0,0 +1,29 @@
+import {Component, Inject, OnInit, Optional} from '@angular/core';
+import {RbacBasedComponent} from "../../RbacBasedComponent";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {RbacService} from "../../../services/rbac/rbac.service";
+import {Action} from "../../../action";
+
+@Component({
+  selector: 'app-picture-dialog-box',
+  templateUrl: './picture-dialog-box.component.html',
+  styleUrls: ['./picture-dialog-box.component.scss']
+})
+export class PictureDialogBoxComponent extends RbacBasedComponent implements OnInit {
+
+  participantPicture: string;
+
+  constructor(public dialogRef: MatDialogRef<PictureDialogBoxComponent>, rbacService: RbacService,
+              @Optional() @Inject(MAT_DIALOG_DATA) public data: { image: string }, public dialog: MatDialog,) {
+    super(rbacService);
+    this.participantPicture = data.image;
+  }
+
+  ngOnInit(): void {
+  }
+
+  closeDialog() {
+    this.dialogRef.close({action: Action.Cancel});
+  }
+
+}
