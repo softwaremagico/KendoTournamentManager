1232e8ac3103cbf63558d5790a64c026f1e197fa
Software Magico <softwaremagico@gmail.com>
2023-01-08 20:58:17 +0100

    Using participant images on accreditations

--- backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/repositories/TournamentImageRepository.java
+++ backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/repositories/TournamentImageRepository.java
@@ -24,24 +24,26 @@ package com.softwaremagico.kt.persistence.repositories;
  * #L%
  */
 
-import com.softwaremagico.kt.persistence.entities.Participant;
-import com.softwaremagico.kt.persistence.entities.ParticipantImage;
+import com.softwaremagico.kt.persistence.entities.Tournament;
+import com.softwaremagico.kt.persistence.entities.TournamentImage;
+import com.softwaremagico.kt.persistence.values.TournamentImageType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional
-public interface ParticipantImageRepository extends JpaRepository<ParticipantImage, Integer> {
+public interface TournamentImageRepository extends JpaRepository<TournamentImage, Integer> {
 
-    Optional<ParticipantImage> findByParticipant(Participant participant);
+    List<TournamentImage> findByTournament(Tournament tournament);
 
-    List<ParticipantImage> findByParticipantIn(Collection<Participant> participants);
+    Optional<TournamentImage> findByTournamentAndImageType(Tournament tournament, TournamentImageType imageType);
 
-    int deleteByParticipant(Participant participant);
+    int deleteByTournament(Tournament tournament);
+
+    int deleteByTournamentAndImageType(Tournament tournament, TournamentImageType imageType);
 
 }
