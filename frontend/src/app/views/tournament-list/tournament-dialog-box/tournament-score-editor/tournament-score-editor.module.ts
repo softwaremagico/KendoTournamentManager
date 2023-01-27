bd0a9f606f270238d7db9afb371d5cdd9bc132d3
Software Magico <softwaremagico@gmail.com>
2023-01-27 17:43:22 +0100

    Adding custom score points

--- frontend/src/app/views/tournament-list/tournament-dialog-box/tournament-score-editor/tournament-score-editor.module.ts
+++ frontend/src/app/views/tournament-list/tournament-dialog-box/tournament-score-editor/tournament-score-editor.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {TournamentScoreEditorComponent} from "./tournament-score-editor.component";
-import {TournamentImageSelectorComponent} from "../tournament-image-selector/tournament-image-selector.component";
-
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatSpinnerOverlayModule} from "../../../../components/mat-spinner-overlay/mat-spinner-overlay.module";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDialogModule} from "@angular/material/dialog";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {RbacModule} from "../../../../pipes/rbac-pipe/rbac.module";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
@@ -11,7 +18,16 @@ import {TournamentImageSelectorComponent} from "../tournament-image-selector/tou
     TournamentScoreEditorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatSpinnerOverlayModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    TranslateModule,
+    MatInputModule,
+    MatButtonModule,
+    RbacModule,
+    MatIconModule
   ]
 })
 export class TournamentScoreEditorModule { }
