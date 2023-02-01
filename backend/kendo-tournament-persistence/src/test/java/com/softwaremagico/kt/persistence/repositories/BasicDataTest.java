77866cb3148c3b4fb7d0a5f6ae3a2ab3760feeab
Software Magico <softwaremagico@gmail.com>
2023-02-01 19:14:25 +0100

    Autogenerate balanced teams depending on the score

--- backend/kendo-tournament-persistence/src/test/java/com/softwaremagico/kt/persistence/repositories/BasicDataTest.java
+++ backend/kendo-tournament-persistence/src/test/java/com/softwaremagico/kt/persistence/repositories/BasicDataTest.java
@@ -8,17 +8,17 @@ package com.softwaremagico.kt.persistence.repositories;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -86,8 +86,8 @@ public abstract class BasicDataTest extends AbstractTestNGSpringContextTests {
         return members;
     }
 
-    protected Tournament createTournament() {
-        return tournamentRepository.save(new Tournament(TOURNAMENT_NAME, 1, MEMBERS, TournamentType.LEAGUE, null));
+    protected Tournament createTournament(String tournamentName) {
+        return tournamentRepository.save(new Tournament(tournamentName, 1, MEMBERS, TournamentType.LEAGUE, null));
     }
 
     protected List<Role> createRoles(List<Participant> members, Tournament tournament) {
@@ -145,9 +145,13 @@ public abstract class BasicDataTest extends AbstractTestNGSpringContextTests {
     }
 
     protected void populateData() {
+        populateData(TOURNAMENT_NAME);
+    }
+
+    protected void populateData(String tournamentName) {
         club = createClub();
         members = createParticipants(club);
-        tournament = createTournament();
+        tournament = createTournament(tournamentName);
         roles = createRoles(members, tournament);
         teams = createTeams(members, tournament);
         group = createGroup(tournament, teams);
