4a44e9005b996767f75143a39ad9868d28c14444
Software Magico <softwaremagico@gmail.com>
2023-02-23 20:15:58 +0100

    Achievement first icons

---
+++ frontend/src/app/components/achievement-tile/achievement-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-achievement-tile',
+  templateUrl: './achievement-tile.component.html',
+  styleUrls: ['./achievement-tile.component.scss']
+})
+export class AchievementTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
