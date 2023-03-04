ec1cdfc1be5420ac69198dba2de3a34594ad9990
Software Magico <softwaremagico@gmail.com>
2023-03-04 17:33:09 +0100

    Adding BillyTheKid achievement logic

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/providers/DuelProvider.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/providers/DuelProvider.java
@@ -67,6 +67,11 @@ public class DuelProvider extends CrudProvider<Duel, Integer, DuelRepository> {
         return repository.findByOnlyScore(tournament, Collections.singleton(score));
     }
 
+    public List<Duel> findByScoreDuration(Tournament tournament, int scoreMaxDuration) {
+        return repository.findByTournamentAndCompetitor1ScoreTimeLessThanEqualOrCompetitor2ScoreTimeLessThanEqual(
+                tournament, scoreMaxDuration, scoreMaxDuration);
+    }
+
     @CacheEvict(allEntries = true, value = {"duelsDurationAverage"})
     @Scheduled(fixedDelay = 60 * 10 * 1000)
     public void reportCacheEvict() {
