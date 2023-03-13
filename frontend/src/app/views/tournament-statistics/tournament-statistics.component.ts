71686aea38ee62a35bae313c34c515d863a96c7b
Software Magico <softwaremagico@gmail.com>
2023-03-13 17:42:24 +0100

    Stack Bar char close

--- frontend/src/app/views/tournament-statistics/tournament-statistics.component.ts
+++ frontend/src/app/views/tournament-statistics/tournament-statistics.component.ts
@@ -5,6 +5,7 @@ import {RbacService} from "../../services/rbac/rbac.service";
 import {SystemOverloadService} from "../../services/notifications/system-overload.service";
 import {BarChartData} from "../../components/charts/bar-chart/bar-chart-data";
 import {Score} from "../../models/score";
+import {StackedBarsChartData} from "../../components/charts/stacked-bars-chart/stacked-bars-chart-data";
 
 @Component({
   selector: 'app-tournament-statistics',
@@ -22,6 +23,13 @@ export class TournamentStatisticsComponent extends RbacBasedComponent implements
       new BarChartData(Score.MEN, 5),
       new BarChartData(Score.KOTE, 5),
       new BarChartData(Score.DO, 3)
+    ];
+
+  public scoresStacked: StackedBarsChartData[] =
+    [
+      new StackedBarsChartData(Score.MEN, ["A", "B", "C", "D"], [5, 4, 3, 2]),
+      new StackedBarsChartData(Score.KOTE, ["A", "B", "C", "D"], [1, 2, 3, 4]),
+      new StackedBarsChartData(Score.DO, ["A", "B", "C", "D"], [6, 7, 8, 9])
     ];
 
   ngOnInit(): void {
