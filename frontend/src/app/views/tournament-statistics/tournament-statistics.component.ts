c3e262b14d4a58211731bb102b66f0d1da209100
Software Magico <softwaremagico@gmail.com>
2023-03-15 13:26:50 +0100

    Added multiple line chart

--- frontend/src/app/views/tournament-statistics/tournament-statistics.component.ts
+++ frontend/src/app/views/tournament-statistics/tournament-statistics.component.ts
@@ -6,7 +6,7 @@ import {SystemOverloadService} from "../../services/notifications/system-overloa
 import {PieChartData} from "../../components/charts/pie-chart/pie-chart-data";
 import {Score} from "../../models/score";
 import {BarChartData} from "../../components/charts/bar-chart/bar-chart-data";
-import {LineChartData} from "../../components/charts/line-chart/line-chart-data";
+import {LineChartData, LineChartDataElement} from "../../components/charts/line-chart/line-chart-data";
 
 @Component({
   selector: 'app-tournament-statistics',
@@ -18,6 +18,8 @@ export class TournamentStatisticsComponent extends RbacBasedComponent implements
   public pieChartData: PieChartData = PieChartData.fromArray([[Score.MEN, 5], [Score.DO, 4], [Score.KOTE, 1]]);
   public barChartData: BarChartData = BarChartData.fromArray([[Score.MEN, 5], [Score.DO, 4], [Score.KOTE, 1]]);
   public lineChartData: LineChartData = LineChartData.fromArray([[Score.MEN, 5], [Score.DO, 4], [Score.KOTE, 1]]);
+  public multipleLineChartData: LineChartData = LineChartData.fromMultipleDataElements([new LineChartDataElement([[Score.MEN, 5], [Score.DO, 4], [Score.KOTE, 1]], "Tournament1"),
+    new LineChartDataElement([[Score.MEN, 1], [Score.DO, 2], [Score.KOTE, 3]], "Tournament2")]);
 
   constructor(private router: Router, rbacService: RbacService, private systemOverloadService: SystemOverloadService) {
     super(rbacService);
