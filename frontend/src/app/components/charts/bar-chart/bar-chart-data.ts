b2190b3d7ca2e6f14b7055b785740465a33816bd
Software Magico <softwaremagico@gmail.com>
2023-03-15 12:11:45 +0100

    Changed line and bar chart to apex charts

--- frontend/src/app/components/charts/bar-chart/bar-chart-data.ts
+++ frontend/src/app/components/charts/bar-chart/bar-chart-data.ts
@@ -1,9 +1,53 @@
 export class BarChartData {
-  key: string;
-  value: number;
+  name: string | undefined;
+  elements: BarChartDataElement[];
 
-  constructor(key: string, value: number) {
-    this.key = key;
-    this.value = value;
+  public static fromArray(elements: [string, number][]): BarChartData {
+    const barChartData: BarChartData = new BarChartData();
+    barChartData.elements = [];
+    for (const element of elements) {
+      barChartData.elements.push(new BarChartDataElement(element[0], element[1]));
+    }
+    return barChartData;
+  }
+
+  public static fromDataElements(elements: BarChartDataElement[]): BarChartData {
+    const barChartData: BarChartData = new BarChartData();
+    barChartData.elements = elements;
+    return barChartData;
+  }
+
+  constructor(name?: string) {
+    this.name = name;
+  }
+
+  getLabels(): string[] {
+    return this.elements.map(e => e.x);
+  }
+
+  getValues(): number[] {
+    return this.elements.map(e => e.y);
+  }
+
+  getData(): Data[] {
+    return [{
+      name: this.name ? this.name : "",
+      data: this.getValues()
+    }]
   }
 }
+
+export class BarChartDataElement {
+  x: string;
+  y: number;
+
+  constructor(x: string, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export type Data = {
+  name: string,
+  data: number[]
+};
