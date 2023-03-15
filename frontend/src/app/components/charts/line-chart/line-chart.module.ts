34e1bde4b4708c3a340e0e8aff9eee2261457154
Software Magico <softwaremagico@gmail.com>
2023-03-15 12:39:38 +0100

    Added line chart

--- frontend/src/app/components/charts/line-chart/line-chart.module.ts
+++ frontend/src/app/components/charts/line-chart/line-chart.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LineChartComponent} from "./line-chart.component";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 
 @NgModule({
   declarations: [LineChartComponent],
   exports: [LineChartComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgApexchartsModule
   ]
 })
 export class LineChartModule {
