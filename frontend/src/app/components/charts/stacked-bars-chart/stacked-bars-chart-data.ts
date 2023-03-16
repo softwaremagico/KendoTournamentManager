d81e51b3a07d752356f0e3137bcb290edcdeaf77
Software Magico <softwaremagico@gmail.com>
2023-03-16 13:22:17 +0100

    Renamed some data

--- frontend/src/app/components/charts/stacked-bars-chart/stacked-bars-chart-data.ts
+++ frontend/src/app/components/charts/stacked-bars-chart/stacked-bars-chart-data.ts
@@ -23,12 +23,12 @@ export class StackedBarChartData {
     return this.elements.map(e => e.name);
   }
 
-  getData(): Data[] {
-    const data: Map<string, Data> = new Map<string, Data>();
+  getData(): StackedBarsData[] {
+    const data: Map<string, StackedBarsData> = new Map<string, StackedBarsData>();
     for (const element of this.elements) {
       for (const point of element.points) {
         if (data.get(point[0]) === undefined) {
-          data.set(point[0], new Data());
+          data.set(point[0], new StackedBarsData());
         }
         data.get(point[0])!.name = point[0];
         data.get(point[0])!.data.push(point[1]);
@@ -53,7 +53,7 @@ export class StackedBarChartDataElement {
   }
 }
 
-export class Data {
+export class StackedBarsData {
   name: string;
   data: number[] = [];
   color: string; //Color be set on the chart component
