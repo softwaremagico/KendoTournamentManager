86fb9074245c469a55321943bdc52f0c16a31cb8
Software Magico <softwaremagico@gmail.com>
2023-03-28 10:23:45 +0200

    Enabling caching.

--- backend/kendo-tournament-core/src/test/java/com/softwaremagico/kt/core/KendoTournamentServer.java
+++ backend/kendo-tournament-core/src/test/java/com/softwaremagico/kt/core/KendoTournamentServer.java
@@ -26,8 +26,10 @@ package com.softwaremagico.kt.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
+@EnableCaching
 @SpringBootApplication
 @Service
 public class KendoTournamentServer {
