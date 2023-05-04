165f15af9df2ab2ba991e93ecd1d105dac6453ab
Software Magico <softwaremagico@gmail.com>
2023-05-04 21:32:05 +0100

    Fixed appex charts night mode

--- frontend/src/app/components/charts/radial-chart/radial-chart.component.ts
+++ frontend/src/app/components/charts/radial-chart/radial-chart.component.ts
@@ -1,10 +1,19 @@
 import {Component, Input, ViewChild} from '@angular/core';
-import {ApexChart, ApexFill, ApexLegend, ApexPlotOptions, ApexTitleSubtitle, ChartComponent} from "ng-apexcharts";
+import {
+  ApexChart,
+  ApexFill,
+  ApexLegend,
+  ApexPlotOptions,
+  ApexTitleSubtitle,
+  ApexTooltip,
+  ChartComponent
+} from "ng-apexcharts";
 import {Colors} from "../colors";
 import {RadialChartData} from "./radial-chart-data";
-import {CustomChartComponent} from "../CustomChartComponent";
+import {CustomChartComponent} from "../custom-chart-component";
 import {DarkModeService} from "../../../services/notifications/dark-mode.service";
 import {UserSessionService} from "../../../services/user-session.service";
+import {ApexTheme} from "ng-apexcharts/lib/model/apex-types";
 
 type RadialChartOptions = {
   series: number[];
@@ -13,8 +22,10 @@ type RadialChartOptions = {
   fill: ApexFill;
   chart: ApexChart;
   plotOptions: ApexPlotOptions;
+  tooltip: ApexTooltip;
   title: ApexTitleSubtitle;
   legend: ApexLegend;
+  theme: ApexTheme;
 };
 
 
@@ -63,78 +74,45 @@ export class RadialChartComponent extends CustomChartComponent {
   protected setProperties(): void {
     this.chartOptions = {
       colors: this.colors,
-      chart: {
-        width: this.width,
-        type: "radialBar",
-        toolbar: {
-          show: this.showToolbar,
-        },
-        dropShadow: {
-          enabled: this.shadow,
-          color: '#000',
-          top: 18,
-          left: 7,
-          blur: 10,
-          opacity: 0.2
-        },
-      },
+      chart: this.getChart('radialBar', this.width, this.shadow, this.showToolbar),
       series: this.data.getValues(),
       labels: this.data.getLabels(),
-      fill: {
-        type: this.fill,
-        gradient: {
-          shade: "light",
-          shadeIntensity: 0.4,
-          inverseColors: false,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 50, 53, 91]
-        }
-      },
-      plotOptions: {
-        radialBar: {
-          startAngle: this.startAngle,
-          endAngle: this.endAngle,
-          dataLabels: {
-            name: {
-              fontSize: "22px"
-            },
-            value: {
-              offsetY: 5,
-              fontSize: "16px"
-            },
-            total: {
-              show: true,
-              label: "Total",
-              color: "#000000",
-              formatter: (w: any) => {
-                return (Math.round((this.data.getTotal() / this.data.getValues().length) * 100) / 100).toFixed(2) + "%";
-              }
-            }
-          },
-          hollow: {
-            size: this.innerCirclePercentage + "%"
-          }
-        }
-      },
-      title: {
-        text: this.title,
-        align: this.titleAlignment,
-        style: {
-          fontSize: '14px',
-          fontWeight: 'bold',
-          fontFamily: 'Roboto',
-          color: this.titleTextColor
-        },
-      },
-      legend: {
-        position: this.legendPosition,
-        labels: {
-          colors: this.legendTextColor,
-          useSeriesColors: false
-        },
-      },
+      fill: this.getFill(this.fill),
+      plotOptions: this.getPlotOptions(),
+      tooltip: this.getTooltip(),
+      title: this.getTitle(this.title, this.titleAlignment),
+      legend: this.getLegend(this.legendPosition),
+      theme: this.getTheme()
     };
+  }
+
+  protected getPlotOptions(): ApexPlotOptions {
+    return {
+      radialBar: {
+        startAngle: this.startAngle,
+        endAngle: this.endAngle,
+        dataLabels: {
+          name: {
+            fontSize: "22px"
+          },
+          value: {
+            offsetY: 5,
+            fontSize: "16px"
+          },
+          total: {
+            show: true,
+            label: "Total",
+            color: "#000000",
+            formatter: (w: any) => {
+              return (Math.round((this.data.getTotal() / this.data.getValues().length) * 100) / 100).toFixed(2) + "%";
+            }
+          }
+        },
+        hollow: {
+          size: this.innerCirclePercentage + "%"
+        }
+      }
+    }
   }
 
   update(data: RadialChartData) {
