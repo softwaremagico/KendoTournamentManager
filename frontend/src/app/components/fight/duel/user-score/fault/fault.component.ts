e422eaa11bb9d83055d5ba61a1bbc22e5585a90f
Software Magico <softwaremagico@gmail.com>
2023-05-06 22:27:07 +0200

    Showing times as tooltip on scores and faults

--- frontend/src/app/components/fight/duel/user-score/fault/fault.component.ts
+++ frontend/src/app/components/fight/duel/user-score/fault/fault.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Duel} from "../../../../../models/duel";
 import {DuelService} from "../../../../../services/duel.service";
 import {MessageService} from "../../../../../services/message.service";
@@ -10,7 +10,7 @@ import {ScoreUpdatedService} from "../../../../../services/notifications/score-u
   templateUrl: './fault.component.html',
   styleUrls: ['./fault.component.scss']
 })
-export class FaultComponent implements OnInit {
+export class FaultComponent implements OnInit, OnChanges {
 
   @Input()
   duel: Duel;
@@ -21,6 +21,8 @@ export class FaultComponent implements OnInit {
   @Input()
   swapTeams: boolean;
 
+  timeRepresentation: string | undefined;
+
   constructor(private duelService: DuelService, private scoreUpdatedService: ScoreUpdatedService, private messageService: MessageService) {
   }
 
@@ -28,44 +30,62 @@ export class FaultComponent implements OnInit {
     // This is intentional
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['duel'] || changes['left'] || changes['swapTeams']) {
+      this.setTime();
+    }
+  }
+
   private setFault(fault: boolean): boolean {
     if ((this.left && !this.swapTeams) || (!this.left && this.swapTeams)) {
       if (!fault || !this.duel.competitor1Fault) {
         this.duel.competitor1Fault = fault;
-        this.duel.competitor1FaultTime = this.duel.duration!;
+        if (fault) {
+          this.duel.competitor1FaultTime = this.duel.duration!;
+        } else {
+          this.duel.competitor1FaultTime = undefined;
+        }
       } else {
         this.duel.competitor1Fault = false;
+        this.duel.competitor1FaultTime = undefined;
         if (this.duel.competitor2Score.length < 2) {
           this.duel.competitor2Score.push(Score.HANSOKU);
           this.duel.competitor2ScoreTime.push(this.duel.duration!);
           this.scoreUpdatedService.isScoreUpdated.next(this.duel);
         } else {
           this.messageService.warningMessage("scoreNotAdded");
+          this.setTime();
           return false;
         }
       }
     } else {
       if (!fault || !this.duel.competitor2Fault) {
         this.duel.competitor2Fault = fault;
-        this.duel.competitor2FaultTime = this.duel.duration!;
+        if (fault) {
+          this.duel.competitor2FaultTime = this.duel.duration!;
+        } else {
+          this.duel.competitor2FaultTime = undefined;
+        }
       } else {
         this.duel.competitor2Fault = false;
+        this.duel.competitor2FaultTime = undefined;
         if (this.duel.competitor1Score.length < 2) {
           this.duel.competitor1Score.push(Score.HANSOKU);
           this.duel.competitor1ScoreTime.push(this.duel.duration!);
           this.scoreUpdatedService.isScoreUpdated.next(this.duel);
         } else {
           this.messageService.warningMessage("scoreNotAdded");
+          this.setTime();
           return false;
         }
       }
     }
+    this.setTime();
     return true;
   }
 
   updateFault(fault: boolean) {
     const faultAdded: boolean = this.setFault(fault);
-    this.duel.finishedAt = undefined;
     this.duelService.update(this.duel).subscribe(duel => {
       if (faultAdded) {
         this.messageService.infoMessage('infoFaultUpdated');
@@ -74,4 +94,22 @@ export class FaultComponent implements OnInit {
     });
   }
 
+  setTime() {
+    let seconds: number | undefined = (this.left && !this.swapTeams) || (!this.left && this.swapTeams) ?
+      this.duel.competitor1FaultTime : this.duel.competitor2FaultTime;
+    if (seconds) {
+      const minutes: number | undefined = seconds ? Math.floor(seconds / 60) : undefined;
+      seconds = seconds % 60;
+      let text: string = "";
+      if (minutes) {
+        text += minutes + "' ";
+      }
+      if (seconds) {
+        text += seconds + '"';
+      }
+      this.timeRepresentation = text;
+    } else {
+      this.timeRepresentation = undefined;
+    }
+  }
 }
