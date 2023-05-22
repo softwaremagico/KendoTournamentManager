33907753d26fb7b29ac7680f8d75fd593ea8b65b
Software Magico <softwaremagico@gmail.com>
2023-05-22 23:09:22 +0100

    Implementing GCM encryption

--- backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/encryption/CipherInitializer.java
+++ backend/kendo-tournament-persistence/src/main/java/com/softwaremagico/kt/persistence/encryption/CipherInitializer.java
@@ -26,69 +26,89 @@ package com.softwaremagico.kt.persistence.encryption;
 
 import com.softwaremagico.kt.logger.EncryptorLogger;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.Base64;
 
 import static com.softwaremagico.kt.persistence.encryption.KeyProperty.databaseEncryptionKey;
 
 public class CipherInitializer {
 
-    private static final String CIPHER_INSTANCE_NAME = "AES/CBC/PKCS5Padding";
+    private static final String CIPHER_INSTANCE_NAME = "AES/GCM/NoPadding";
     private static final String SECRET_KEY_ALGORITHM = "AES";
+    private static final String SECRET_KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA256";
+    private static final int AES_KEY_SIZE = 256;
     private static final String SECRET_MESSAGE_DIGEST_ALGORITHM = "SHA-256";
+    private static final int TAG_LENGTH_BIT = 128;
+    public static final int GCM_IV_LENGTH = 12;
+    private static final int SALT_LENGTH_BYTE = 16;
     private static final SecureRandom random = new SecureRandom();
     private Cipher cipher;
-    private IvParameterSpec ivSpec;
     private SecretKeySpec keySpec;
 
+    // AES-GCM needs GCMParameterSpec
     public String encrypt(String input) throws InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException {
-        getCipher().init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-        final byte[] encryptedBytes = getCipher().doFinal(input.getBytes(StandardCharsets.UTF_8));
+            BadPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        final byte[] salt = getRandomNonce(SALT_LENGTH_BYTE);
+        final byte[] iv = getRandomNonce(GCM_IV_LENGTH);
+        final Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE_NAME);
+        cipher.init(Cipher.ENCRYPT_MODE, getAESKey(salt), new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+        final byte[] encryptedText = cipher.doFinal(input.getBytes(StandardCharsets.UTF_8));
+        // prefix IV length + IV bytes to cipher text
+        final byte[] encryptedBytes = ByteBuffer.allocate(iv.length + salt.length + encryptedText.length)
+                .put(iv)
+                .put(salt)
+                .put(encryptedText)
+                .array();
         final String encodedValue = Base64.getEncoder().encodeToString(encryptedBytes);
         EncryptorLogger.debug(this.getClass().getName(), "Encrypted value for '{}' is '{}'.", input, encodedValue);
         return encodedValue;
     }
 
     public String decrypt(String encrypted) throws BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException,
-            InvalidKeyException {
-        getCipher().init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+            InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException {
         final byte[] encryptedBytes = Base64.getDecoder().decode(encrypted.getBytes(StandardCharsets.UTF_8));
+
+        // get back the iv and salt from the cipher text
+        final ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedBytes);
+        // 16 bytes salt
+        final byte[] iv = new byte[GCM_IV_LENGTH];
+        byteBuffer.get(iv);
+        final byte[] salt = new byte[SALT_LENGTH_BYTE];
+        byteBuffer.get(salt);
+        final byte[] cipherText = new byte[byteBuffer.remaining()];
+        byteBuffer.get(cipherText);
+
+        final Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE_NAME);
+        cipher.init(Cipher.DECRYPT_MODE, getAESKey(salt), new GCMParameterSpec(TAG_LENGTH_BIT, iv));
         final byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
         final String decrypted = new String(decryptedBytes, StandardCharsets.UTF_8);
         EncryptorLogger.debug(this.getClass().getName(), "Decrypted value for '{}' is '{}'.", encrypted, decrypted);
         return decrypted;
     }
 
-    private Cipher getCipher() {
-        if (cipher == null) {
-            try {
-                cipher = Cipher.getInstance(CIPHER_INSTANCE_NAME);
-                final byte[] iv = new byte[cipher.getBlockSize()];
-                random.nextBytes(iv);
-                ivSpec = new IvParameterSpec(iv);
+    public static byte[] getRandomNonce(int numBytes) {
+        final byte[] nonce = new byte[numBytes];
+        random.nextBytes(nonce);
+        return nonce;
+    }
 
-                // hash keyString with SHA-256 and crop the output to 128-bit for key
-                final MessageDigest digest = MessageDigest.getInstance(SECRET_MESSAGE_DIGEST_ALGORITHM);
-                digest.update(databaseEncryptionKey.getBytes(StandardCharsets.UTF_8));
-                final byte[] key = new byte[16];
-                System.arraycopy(digest.digest(), 0, key, 0, key.length);
-                keySpec = new SecretKeySpec(key, SECRET_KEY_ALGORITHM);
-            } catch (Exception e) {
-                EncryptorLogger.severe(this.getClass().getName(), "invalid cipher algorithm selected");
-                EncryptorLogger.errorMessage(this.getClass().getName(), e);
-                System.exit(0);
-            }
-        }
-        return cipher;
+    // AES secret key
+    public static SecretKeySpec getAESKey(byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        final SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY_ALGORITHM);
+        final KeySpec spec = new PBEKeySpec(databaseEncryptionKey.toCharArray(), salt,
+                65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), SECRET_KEY_ALGORITHM);
+
     }
 }
