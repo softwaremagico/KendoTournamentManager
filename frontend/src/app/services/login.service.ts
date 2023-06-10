f47ced3656ed129c4409e9d56193e37fea23e0fc
Software Magico <softwaremagico@gmail.com>
2023-06-10 18:55:42 +0100

    Frontend renews JWT

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {CookieService} from "ngx-cookie-service";
@@ -13,7 +13,9 @@ import {EnvironmentService} from "../environment.service";
 })
 export class LoginService {
 
-  private baseUrl: string = this.environmentService.getBackendUrl() + '/auth/public';
+  private baseUrl: string = this.environmentService.getBackendUrl() + '/auth';
+  private static readonly FIRST_RENEW_JWT: number = 1000;
+  private interval: NodeJS.Timeout | null;
 
   httpOptions: { headers: HttpHeaders };
 
@@ -23,7 +25,7 @@ export class LoginService {
   }
 
   login(username: string, password: string): Observable<AuthenticatedUser> {
-    const url: string = `${this.baseUrl}/login`;
+    const url: string = `${this.baseUrl}/public/login`;
     return this.http.post<AuthenticatedUser>(url, new AuthRequest(username, password), {
       headers: new HttpHeaders({'Content-Type': 'application/json'}),
       responseType: 'json',
@@ -32,6 +34,7 @@ export class LoginService {
       .pipe(
         map((response: any) => {
           response.body.jwt = response.headers.get('Authorization');
+          response.body.expires = response.headers.get('Expires');
           return response.body;
         }));
   }
@@ -39,10 +42,11 @@ export class LoginService {
   logout(): void {
     this.cookies.delete("jwt");
     this.cookies.delete("selectedLanguage");
+    this.cookies.delete("jwt_expires");
     sessionStorage.clear();
   }
 
-  private refreshHeader() {
+  private refreshHeader(): void {
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -51,12 +55,54 @@ export class LoginService {
     };
   }
 
-  setJwtValue(token: string) {
+  public setJwtValue(token: string, expires: number): void {
     this.cookies.set("jwt", token);
+    this.cookies.set("jwt_expires", expires.toString());
     this.refreshHeader();
   }
 
-  getJwtValue(): string {
+  public getJwtValue(): string {
     return this.cookies.get("jwt");
+  }
+
+  getJwtExpirationValue(): number {
+    return Number(this.cookies.get("jwt_expires"));
+  }
+
+  public autoRenewToken(jwt: string, expiration: number, callback: (token: string, expiration: number) => void): void {
+    if (this.interval != null) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+    this.setIntervalRenew(jwt, expiration, callback);
+  }
+
+  private setIntervalRenew(jwt: string, timeout: number, callback: (jwt: string, expiration: number) => void): void {
+    this.interval = setInterval((): void => {
+      //Set current JWT.
+      this.setJwtValue(jwt, timeout);
+      this.renew(jwt).subscribe(
+        (res: HttpResponse<AuthenticatedUser>): void => {
+          const authToken: string | null = res.headers.get('authorization');
+          let expiration: number = Number(res.headers.get('expires'));
+          if (!authToken || !expiration) {
+            throw new Error('Server returned invalid response');
+          }
+          if (isNaN(expiration)) {
+            throw new Error('Server returned invalid expiration time');
+          }
+          expiration = expiration - (new Date()).getTime();
+          console.log(`Next token expiration time: ${expiration}`);
+          callback(authToken, expiration);
+          this.setIntervalRenew(authToken, expiration, callback);
+        }
+      )
+    }, timeout)
+  }
+
+  private renew(jwt ?: string): Observable<HttpResponse<AuthenticatedUser>> {
+    return jwt ?
+      this.http.get<HttpResponse<AuthenticatedUser>>(`${this.baseUrl}/jwt/renew`, this.httpOptions)
+      : this.http.get<HttpResponse<AuthenticatedUser>>(`${this.baseUrl}/jwt/renew`);
   }
 }
