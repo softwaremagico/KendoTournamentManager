0da9fb32f452e0f4ca6a9649c34708303ed57354
Software Magico <softwaremagico@gmail.com>
2023-07-19 16:37:44 +0200

    Adding error handler

---
+++ frontend/src/app/interceptors/local-error-handler.service.ts
@@ -0,0 +1,21 @@
+import {ErrorHandler, Injectable} from "@angular/core";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MessageService} from "../services/message.service";
+import {LoggerService} from "../services/logger.service";
+
+
+@Injectable()
+export class LocalErrorHandler implements ErrorHandler {
+
+  constructor(private messageService: MessageService, private loggerService: LoggerService) {
+  }
+
+  handleError(error: any): void {
+    //These errors are already handled by HttpErrorInterceptor
+    if (error instanceof HttpErrorResponse) {
+      //Show error
+      this.messageService.errorMessage(`Error connecting to the backend service. ${error.url} failed: ${error ? error.message : ""}`);
+    }
+    //this.loggerService.error(error.message);
+  }
+}
