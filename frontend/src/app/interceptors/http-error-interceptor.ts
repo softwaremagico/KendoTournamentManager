b6786fad1f8ac87ae84a8daafd7f4142b277b4f8
Software Magico <softwaremagico@gmail.com>
2023-07-19 10:59:25 +0200

    Redirecting 401 errors

--- frontend/src/app/interceptors/http-error-interceptor.ts
+++ frontend/src/app/interceptors/http-error-interceptor.ts
@@ -7,21 +7,22 @@ import {LoginService} from "../services/login.service";
 import {MessageService} from "../services/message.service";
 
 @Injectable()
-export class InvalidJwtInterceptor implements HttpInterceptor {
+export class HttpErrorInterceptor implements HttpInterceptor {
 
   constructor(public router: Router, private loginService: LoginService,
               private messageService: MessageService) {
   }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    return next.handle(req).pipe(
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
       catchError((error) => {
-        //If on JWT, the IP is changed, launch a 409 error. 401 and 423 are for invalid or expired jwt. As Jwt is invalid now, logging again.
-        if (error.status === 409 || error.status === 423) {
-          this.loginService.logout();
-          this.messageService.warningMessage("userloggedOutMessage");
-          this.router.navigate(['/login'], {queryParams: {returnUrl: "/tournaments"}});
+
+        if (error.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.error('An error occurred:', error.error.message);
+        } else {
+          // Log error.
+          console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
         }
         return throwError(error.message);
       })
