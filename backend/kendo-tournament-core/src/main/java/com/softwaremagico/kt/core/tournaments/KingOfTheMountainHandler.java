0e6f5f326fa059b42f0f7bddb76ceddaa77349fa
Software Magico <softwaremagico@gmail.com>
2023-07-28 17:17:50 +0200

    License downloaded directly

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/tournaments/KingOfTheMountainHandler.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/tournaments/KingOfTheMountainHandler.java
@@ -6,21 +6,18 @@ package com.softwaremagico.kt.core.tournaments;
  * %%
  * Copyright (C) 2021 - 2023 Softwaremagico
  * %%
- * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
- * <softwaremagico@gmail.com> Valencia (Spain).
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
@@ -142,7 +139,8 @@ public class KingOfTheMountainHandler extends LeagueHandler {
             final DrawResolution drawResolution = DrawResolution.getFromTag(extraProperty.getPropertyValue());
             final Group previousLastGroup = level > 1 ? groupProvider.getGroups(tournament, level - 2).get(0) : null;
             switch (drawResolution) {
-                case BOTH_ELIMINATED -> bothEliminated(existingTeams, teams, teamConverter.reverseAll(ranking.get(0)), tournament);
+                case BOTH_ELIMINATED ->
+                        bothEliminated(existingTeams, teams, teamConverter.reverseAll(ranking.get(0)), tournament);
                 case OLDEST_ELIMINATED -> {
                     if (previousLastGroup == null) {
                         bothEliminated(existingTeams, teams, teamConverter.reverseAll(ranking.get(0)), tournament);
