81a2dade81a6b4d341ff376b4c59d5e86b63c55e
softwaremagico <softwaremagico@gmail.com>
2023-08-05 06:10:38 +0200

    generating view

---
+++ frontend/src/app/views/fight-list/tournament-generator/tournament-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-generator',
+  templateUrl: './tournament-generator.component.html',
+  styleUrls: ['./tournament-generator.component.scss']
+})
+export class TournamentGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
