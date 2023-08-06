71130d09f928f8c8250f48d25a9582026aa93022
softwaremagico <softwaremagico@gmail.com>
2023-08-06 07:11:58 +0200

    GroupLink service

--- frontend/src/app/components/tournament-brackets-editor/tournament-brackets-editor.component.ts
+++ frontend/src/app/components/tournament-brackets-editor/tournament-brackets-editor.component.ts
@@ -5,6 +5,9 @@ import {Team} from "../../models/team";
 import {TeamListData} from "../basic/team-list/team-list-data";
 import {TeamService} from "../../services/team.service";
 import {Tournament} from "../../models/tournament";
+import {GroupLink} from "../../models/group-link.model";
+import {GroupLinkService} from "../../services/group-link.service";
+import {GroupService} from "../../services/group.service";
 
 @Component({
   selector: 'app-tournament-brackets-editor',
@@ -15,42 +18,49 @@ export class TournamentBracketsEditorComponent implements OnInit {
 
   @Input()
   tournament: Tournament;
-  @Input()
+  
   groups: Group[];
-  @Input()
+
+  //Level -> Src Group -> Dst Group
   relations: Map<number, { src: number, dest: number }[]>;
 
   teamListData: TeamListData = new TeamListData();
 
   totalTeams: number;
 
-  constructor(private teamService: TeamService) {
+  constructor(private teamService: TeamService, private groupService: GroupService, private groupLinkService: GroupLinkService) {
   }
 
   ngOnInit(): void {
-    // this.teamService.getFromTournament(this.tournament).subscribe((teams: Team[]): void => {
-    //   if (teams) {
-    //     teams.sort(function (a: Team, b: Team) {
-    //       return a.name.localeCompare(b.name);
-    //     });
-    //   }
-    //   this.teamListData.teams = teams;
-    //   this.teamListData.filteredTeams = teams;
-    // });
-    const teams: Team[] = [];
-    teams.push(new Team("Team1"));
-    teams.push(new Team("Team2"));
-    teams.push(new Team("Team3"));
-    teams.push(new Team("Team4"));
-    teams.push(new Team("Team5"));
-    teams.push(new Team("Team6"));
-    teams.push(new Team("Team7"));
-    teams.push(new Team("Team8"));
-    teams.push(new Team("Team9"));
-    this.teamListData.teams = teams;
-    this.totalTeams = teams.length;
-    this.teamListData.filteredTeams = teams;
+    this.teamService.getFromTournament(this.tournament).subscribe((teams: Team[]): void => {
+      if (teams) {
+        teams.sort(function (a: Team, b: Team) {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      this.teamListData.teams = teams;
+      this.teamListData.filteredTeams = teams;
+    });
+    this.groupService.getFromTournament(this.tournament.id!).subscribe((_groups: Group[]): void => {
+      this.groups = _groups;
+    })
+    this.groupLinkService.getFromTournament(this.tournament.id!).subscribe((_groupRelations: GroupLink[]): void => {
+      this.relations = this.convert(_groupRelations);
+    })
   }
+
+
+  convert(groupRelations: GroupLink[]): Map<number, { src: number, dest: number }[]> {
+    const relations: Map<number, { src: number, dest: number }[]> = new Map();
+    for (const groupLink of groupRelations) {
+      if (!relations.get(groupLink.source!.level!)) {
+        relations.set(groupLink.source!.level!, []);
+      }
+      relations.get(groupLink.source!.level!)?.push({src: groupLink.source!.id!, dest: groupLink.destination!.id!});
+    }
+    return relations;
+  }
+
 
   removeTeam(event: CdkDragDrop<Team[], any>): void {
     transferArrayItem(
