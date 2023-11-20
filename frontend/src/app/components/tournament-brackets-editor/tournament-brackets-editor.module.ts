f65f11586fcfbabbd341dd4e72ce5d6b9b160c5e
Software Magico <softwaremagico@gmail.com>
2023-11-20 13:50:25 +0100

    Adding random teams to groups

--- frontend/src/app/components/tournament-brackets-editor/tournament-brackets-editor.module.ts
+++ frontend/src/app/components/tournament-brackets-editor/tournament-brackets-editor.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TournamentBracketsEditorComponent } from './tournament-brackets-editor.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TournamentBracketsEditorComponent} from './tournament-brackets-editor.component';
 import {TournamentBracketsModule} from "./tournament-brackets/tournament-brackets.module";
 import {DragDropModule} from "@angular/cdk/drag-drop";
 import {TeamCardModule} from "../team-card/team-card.module";
 import {TeamListModule} from "../basic/team-list/team-list.module";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
-
+import {RbacModule} from "../../pipes/rbac-pipe/rbac.module";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatDialogModule} from "@angular/material/dialog";
 
 
 @NgModule({
@@ -17,14 +19,17 @@ import {MatIconModule} from "@angular/material/icon";
   exports:[
     TournamentBracketsEditorComponent
   ],
-    imports: [
-        CommonModule,
-        TournamentBracketsModule,
-        DragDropModule,
-        TeamCardModule,
-        TeamListModule,
-        MatButtonModule,
-        MatIconModule
-    ]
+  imports: [
+    CommonModule,
+    TournamentBracketsModule,
+    DragDropModule,
+    TeamCardModule,
+    TeamListModule,
+    MatButtonModule,
+    MatIconModule,
+    RbacModule,
+    TranslateModule,
+    MatDialogModule
+  ]
 })
 export class TournamentBracketsEditorModule { }
