601f425fece1901ba1f5b002ebf927f3eac193c6
Software Magico <softwaremagico@gmail.com>
2024-01-03 09:40:10 +0100

    Corrected lines position

--- frontend/src/app/components/tournament-brackets-editor/tournament-brackets/shiaijo/shiaijo.component.ts
+++ frontend/src/app/components/tournament-brackets-editor/tournament-brackets/shiaijo/shiaijo.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {Group} from "../../../../models/group";
-import {TournamentBracketsComponent} from "../tournament-brackets.component";
 import {GroupsUpdatedService} from "../groups-updated.service";
+import {BracketsMeasures} from "../brackets-measures";
+import {TournamentBracketsComponent} from "../tournament-brackets.component";
 
 @Component({
   selector: 'app-shiaijo',
@@ -21,16 +22,15 @@ export class ShiaijoComponent {
   groupsByLevel: Map<number, Group[]>;
 
   @Input()
-  groups: Group[];
-
-  @Input()
   totalShiaijos: number | undefined;
 
   @Input()
-  shiajo: number;
+  shiaijo: number;
 
   @Input()
   color: string = "#001239";
+
+  groups: Group[];
 
 
   totalTeams: number;
@@ -47,14 +47,20 @@ export class ShiaijoComponent {
   }
 
   ngOnInit(): void {
+    this.groups = this.getGroupsInShiaijo(this.shiaijo);
     this.groupsUpdatedService.areTotalTeamsNumberUpdated.subscribe((_totalTeams: number): void => {
       this.totalTeams = _totalTeams;
       this.generateCoordinates();
     })
+    this.groupsUpdatedService.areGroupsUpdated.subscribe((_groups: Group[]): void => {
+      this.groupsByLevel = TournamentBracketsComponent.convert(_groups);
+      this.groups = this.getGroupsInShiaijo(this.shiaijo);
+      this.generateCoordinates();
+    });
   }
 
   generateCoordinates(): void {
-    const lastGroupIndex: number = this.getLastGroupIndex(this.shiajo);
+    const lastGroupIndex: number = this.getLastGroupIndex(this.shiaijo);
 
     this.x1 = 0;
     this.y1 = this.getYCoordinate(lastGroupIndex);
@@ -63,13 +69,31 @@ export class ShiaijoComponent {
     this.height = Math.max(this.y2, this.y1);
     this.width = Math.max(this.x2, this.x1);
 
-    console.log(this.groups);
-    console.log(this.shiajo, '***REMOVED***>', lastGroupIndex, ': ', this.getGroupHigh(0, lastGroupIndex + 1));
+    console.log(this.shiaijo, '***REMOVED***>', lastGroupIndex, ': ', this.getGroupHigh2(0, lastGroupIndex + 1));
+  }
+
+  getGroupHigh2(level: number, index: number): number {
+    if (this.groupsByLevel && this.groupsByLevel!.get(level) && this.groupsByLevel.get(level)![index]) {
+      const estimatedTeams: number = Math.ceil(this.totalTeams / this.groupsByLevel.get(0)!.length);
+      let teams: number = -1;
+      for (const i of this.groupsByLevel.get(level)!.keys()) {
+        if (this.groupsByLevel.get(level)![i].teams.length > teams) {
+          teams = this.groupsByLevel.get(level)![i].teams.length;
+        }
+      }
+
+      if (level == 0 && Math.max(estimatedTeams, teams) > 1) {
+        return Math.max(estimatedTeams, teams) * 60;
+      } else if (teams && teams > 1) {
+        return teams * 60;
+      }
+    }
+    return BracketsMeasures.GROUP_HIGH;
   }
 
   getYCoordinate(groupIndex: number): number {
     return (this.getGroupTopSeparation(0, groupIndex + 1, null)
-        -  (TournamentBracketsComponent.GROUP_SEPARATION / 2));
+      - (BracketsMeasures.GROUP_SEPARATION / 2));
   }
 
   getLastGroupIndex(shiaijo: number): number {
@@ -85,5 +109,12 @@ export class ShiaijoComponent {
       return this.groups.reduce((group1: Group, group2: Group): Group => group1.index > group2.index ? group1 : group2);
     }
     return undefined;
+  }
+
+  getGroupsInShiaijo(shiaijo: number): Group[] {
+    if (this.groupsByLevel.get(0)) {
+      return this.groupsByLevel.get(0)!.filter((_group: Group): boolean => _group.shiaijo == shiaijo);
+    }
+    return [];
   }
 }
