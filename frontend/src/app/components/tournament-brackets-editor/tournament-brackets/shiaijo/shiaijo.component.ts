0dcd5759a2a16f80fb1b5ccacadeb17fa414794f
Software Magico <softwaremagico@gmail.com>
2024-01-03 11:10:22 +0100

    Showing shiaijos on each level

--- frontend/src/app/components/tournament-brackets-editor/tournament-brackets/shiaijo/shiaijo.component.ts
+++ frontend/src/app/components/tournament-brackets-editor/tournament-brackets/shiaijo/shiaijo.component.ts
@@ -26,6 +26,9 @@ export class ShiaijoComponent {
   totalShiaijos: number | undefined;
 
   @Input()
+  level: number;
+
+  @Input()
   shiaijo: number;
 
   @Input()
@@ -48,14 +51,14 @@ export class ShiaijoComponent {
   }
 
   ngOnInit(): void {
-    this.groups = this.getGroupsInShiaijo(this.shiaijo);
+    this.groups = this.getGroupsInShiaijo(this.shiaijo, this.level);
     this.groupsUpdatedService.areTotalTeamsNumberUpdated.subscribe((_totalTeams: number): void => {
       this.totalTeams = _totalTeams;
       this.generateCoordinates();
     })
     this.groupsUpdatedService.areGroupsUpdated.subscribe((_groups: Group[]): void => {
       this.groupsByLevel = TournamentBracketsComponent.convert(_groups);
-      this.groups = this.getGroupsInShiaijo(this.shiaijo);
+      this.groups = this.getGroupsInShiaijo(this.shiaijo, this.level);
       this.generateCoordinates();
     });
   }
@@ -63,38 +66,19 @@ export class ShiaijoComponent {
   generateCoordinates(): void {
     const lastGroupIndex: number = this.getLastGroupIndex(this.shiaijo);
 
-    this.x1 = 0;
+    this.x1 = this.level * (BracketsMeasures.GROUP_WIDTH + BracketsMeasures.LEVEL_SEPARATION) + BracketsMeasures.SHIAIJO_PADDING;
     this.y1 = this.getYCoordinate(lastGroupIndex);
-    this.x2 = 500;
+    this.x2 = this.level * (BracketsMeasures.GROUP_WIDTH + BracketsMeasures.LEVEL_SEPARATION) + BracketsMeasures.GROUP_WIDTH - BracketsMeasures.SHIAIJO_PADDING;
     this.y2 = this.getYCoordinate(lastGroupIndex);
     this.height = Math.max(this.y2, this.y1);
     this.width = Math.max(this.x2, this.x1);
-
-    console.log(this.shiaijo, '--->', lastGroupIndex, ': ', this.getGroupHigh2(0, lastGroupIndex + 1));
-  }
-
-  getGroupHigh2(level: number, index: number): number {
-    if (this.groupsByLevel && this.groupsByLevel!.get(level) && this.groupsByLevel.get(level)![index]) {
-      const estimatedTeams: number = Math.ceil(this.totalTeams / this.groupsByLevel.get(0)!.length);
-      let teams: number = -1;
-      for (const i of this.groupsByLevel.get(level)!.keys()) {
-        if (this.groupsByLevel.get(level)![i].teams.length > teams) {
-          teams = this.groupsByLevel.get(level)![i].teams.length;
-        }
-      }
-
-      if (level == 0 && Math.max(estimatedTeams, teams) > 1) {
-        return Math.max(estimatedTeams, teams) * 60;
-      } else if (teams && teams > 1) {
-        return teams * 60;
-      }
-    }
-    return BracketsMeasures.GROUP_HIGH;
   }
 
   getYCoordinate(groupIndex: number): number {
-    return (this.getGroupTopSeparation(0, groupIndex + 1, null)
-      - (BracketsMeasures.GROUP_SEPARATION / 2));
+    const firstGroupSeparation: number = this.getGroupTopSeparation(this.level, groupIndex, this.groupsByLevel);
+    const secondGroupSeparation: number = this.getGroupTopSeparation(this.level, groupIndex + 1, this.groupsByLevel);
+    const groupHeight: number = this.getGroupHigh(this.level, groupIndex);
+    return ((secondGroupSeparation - firstGroupSeparation - groupHeight) / 2) + firstGroupSeparation + groupHeight;
   }
 
   getLastGroupIndex(shiaijo: number): number {
@@ -112,9 +96,9 @@ export class ShiaijoComponent {
     return undefined;
   }
 
-  getGroupsInShiaijo(shiaijo: number): Group[] {
-    if (this.groupsByLevel.get(0)) {
-      return this.groupsByLevel.get(0)!.filter((_group: Group): boolean => _group.shiaijo == shiaijo);
+  getGroupsInShiaijo(shiaijo: number, level: number): Group[] {
+    if (this.groupsByLevel.get(level)) {
+      return this.groupsByLevel.get(level)!.filter((_group: Group): boolean => _group.shiaijo == shiaijo);
     }
     return [];
   }
@@ -124,6 +108,6 @@ export class ShiaijoComponent {
   }
 
   getNextShiaijoName(): string {
-    return Tournament.SHIAIJO_NAMES[this.shiaijo+1];
+    return Tournament.SHIAIJO_NAMES[this.shiaijo + 1];
   }
 }
