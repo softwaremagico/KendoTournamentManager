9ae0bc0770ba5b06d557bfe4f8cf22c9ffe92d05
Software Magico <softwaremagico@gmail.com>
2024-01-05 11:29:38 +0100

    Trying to put security

--- backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/websockets/BasicWebsocketsTests.java
+++ backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/websockets/BasicWebsocketsTests.java
@@ -1,5 +1,10 @@
 package com.softwaremagico.kt.websockets;
 
+import com.softwaremagico.kt.persistence.entities.AuthenticatedUser;
+import com.softwaremagico.kt.rest.controllers.AuthenticatedUserController;
+import com.softwaremagico.kt.rest.security.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
@@ -13,6 +18,7 @@ import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -28,19 +34,42 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Test(groups = "websockets")
+@AutoConfigureMockMvc(addFilters = false)
 public class BasicWebsocketsTests extends AbstractTestNGSpringContextTests {
+
+    private final static String USER_FIRST_NAME = "Test";
+    private final static String USER_LAST_NAME = "User";
+
+    private static final String USER_NAME = USER_FIRST_NAME + "." + USER_LAST_NAME;
+    private static final String USER_PASSWORD = "password";
+    private static final String[] USER_ROLES = new String[]{"admin", "viewer"};
 
     private static final String TESTING_MESSAGE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis leo purus, vitae finibus felis fringilla eget.";
 
     @LocalServerPort
     private Integer port;
 
+    @Autowired
+    private AuthenticatedUserController authenticatedUserController;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     private WebSocketStompClient webSocketStompClient;
 
-    private final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+    private WebSocketHttpHeaders headers;
+
 
     private String getWsPath() {
-        return String.format("ws://localhost:%d/kendo-tournament-backend/%s", port, WebSocketConfiguration.SOCKET_RECEIVE_PREFIX);
+        return String.format("ws://127.0.0.1:%d/kendo-tournament-backend/%s", port, WebSocketConfiguration.SOCKET_RECEIVE_PREFIX);
+    }
+
+    @BeforeClass
+    public void authentication() {
+       AuthenticatedUser authenticatedUser = authenticatedUserController.createUser(null, USER_NAME, USER_FIRST_NAME, USER_LAST_NAME, USER_PASSWORD, USER_ROLES);
+
+        headers = new WebSocketHttpHeaders();
+        headers.set("Authorization", "Bearer " + jwtTokenUtil.generateAccessToken(authenticatedUser, "127.0.0.1"));
     }
 
 
@@ -53,7 +82,7 @@ public class BasicWebsocketsTests extends AbstractTestNGSpringContextTests {
 //        webSocketStompClient = new WebSocketStompClient(new SockJsClient(
 //                List.of(new WebSocketTransport(new StandardWebSocketClient()))));
 //
-//        StompSession session = webSocketStompClient.connectAsync(String.format("ws://localhost:%d/%s", port, WebSocketConfiguration.SOCKETS_ROOT_URL),
+//        StompSession session = webSocketStompClient.connectAsync(getWsPath(),
 //                new StompSessionHandlerAdapter() {
 //                }).get(1, TimeUnit.SECONDS);
 //
