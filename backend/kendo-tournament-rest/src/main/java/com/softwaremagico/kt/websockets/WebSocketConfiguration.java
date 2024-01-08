ae913a33672d00dd481731b8c5970e4755002fc0
Software Magico <softwaremagico@gmail.com>
2024-01-08 16:52:09 +0100

    Changed to SockJs

--- backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/websockets/WebSocketConfiguration.java
+++ backend/kendo-tournament-rest/src/main/java/com/softwaremagico/kt/websockets/WebSocketConfiguration.java
@@ -23,7 +23,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(SOCKETS_STOMP_URL)
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*").withSockJS();
     }
 
     @Override
--- backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/websockets/BasicWebsocketsTests.java
+++ backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/websockets/BasicWebsocketsTests.java
@@ -18,11 +18,16 @@ import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutionException;
@@ -77,7 +82,12 @@ public class BasicWebsocketsTests extends AbstractTestNGSpringContextTests {
     @BeforeMethod
     public void setup() {
         WebSocketClient webSocketClient = new StandardWebSocketClient();
-        this.webSocketStompClient = new WebSocketStompClient(webSocketClient);
+
+        List<Transport> transports = new ArrayList<>();
+        transports.add(new WebSocketTransport(webSocketClient));
+        SockJsClient sockJsClient = new SockJsClient(transports);
+
+        this.webSocketStompClient = new WebSocketStompClient(sockJsClient);
         this.webSocketStompClient.setMessageConverter(new StringMessageConverter());
     }
 
