d5e729d290aaaa3fb29cda14e45a1676c7bf74db
Software Magico <softwaremagico@gmail.com>
2024-01-08 17:25:39 +0100

    Fixed tests

--- backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/websockets/FightsWebsocketsTests.java
+++ backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/websockets/FightsWebsocketsTests.java
@@ -1,8 +1,30 @@
 package com.softwaremagico.kt.websockets;
 
+/*-
+ * #%L
+ * Kendo Tournament Manager (Rest)
+ * %%
+ * Copyright (C) 2021 - 2024 Softwaremagico
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softwaremagico.kt.core.controller.models.FightDTO;
+import com.softwaremagico.kt.core.providers.AuthenticatedUserProvider;
 import com.softwaremagico.kt.persistence.entities.AuthenticatedUser;
 import com.softwaremagico.kt.rest.controllers.AuthenticatedUserController;
 import com.softwaremagico.kt.rest.security.JwtTokenUtil;
@@ -24,6 +46,7 @@ import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -63,6 +86,9 @@ public class FightsWebsocketsTests extends AbstractTestNGSpringContextTests {
     private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
+    private AuthenticatedUserProvider authenticatedUserProvider;
+
+    @Autowired
     private WebSocketController webSocketController;
 
     @Autowired
@@ -71,6 +97,8 @@ public class FightsWebsocketsTests extends AbstractTestNGSpringContextTests {
     private WebSocketStompClient webSocketStompClient;
 
     private WebSocketHttpHeaders headers;
+
+    private AuthenticatedUser authenticatedUser;
 
     private <T> String toJson(T object) throws JsonProcessingException {
         return objectMapper.writeValueAsString(object);
@@ -92,7 +120,7 @@ public class FightsWebsocketsTests extends AbstractTestNGSpringContextTests {
 
     @BeforeClass
     public void authentication() {
-        AuthenticatedUser authenticatedUser = authenticatedUserController.createUser(null, USER_NAME, USER_FIRST_NAME, USER_LAST_NAME, USER_PASSWORD, USER_ROLES);
+        authenticatedUser = authenticatedUserController.createUser(null, USER_NAME, USER_FIRST_NAME, USER_LAST_NAME, USER_PASSWORD, USER_ROLES);
 
         headers = new WebSocketHttpHeaders();
         headers.set("Authorization", "Bearer " + jwtTokenUtil.generateAccessToken(authenticatedUser, "127.0.0.1"));
@@ -151,6 +179,11 @@ public class FightsWebsocketsTests extends AbstractTestNGSpringContextTests {
 
         await().atMost(1, TimeUnit.SECONDS)
                 .untilAsserted(() -> assertThat(blockingQueue.poll()).isEqualTo(fightDTO));
+    }
+
+    @AfterClass
+    public void deleteUser() {
+        authenticatedUserProvider.delete(authenticatedUser);
     }
 
 }
