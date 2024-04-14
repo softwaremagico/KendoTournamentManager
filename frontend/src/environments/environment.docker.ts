d1ef6470b6c77b93210c3c1193b0f58def530cb5
Software Magico <softwaremagico@gmail.com>
2024-04-14 19:43:05 +0200

    Correcting the websocket url

--- frontend/src/environments/environment.docker.ts
+++ frontend/src/environments/environment.docker.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   backendUrl: "BACKEND_PROTOCOL://MACHINE_DOMAIN/kendo-tournament-backend",
-  websocketsUrl: "WEBSOCKET_PROTOCOL://MACHINE_DOMAIN/kendo-tournament-backend/sockets",
+  websocketsUrl: "WEBSOCKET_PROTOCOL://MACHINE_DOMAIN/kendo-tournament-backend/websockets",
   websocketsTopicPrefix: "/topic"
 };
