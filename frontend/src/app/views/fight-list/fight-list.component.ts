1c55698785505bceb7d5d0edb8271446d024f445
Software Magico <softwaremagico@gmail.com>
2024-05-13 10:06:08 +0200

    Adding fusen-gachi

--- frontend/src/app/views/fight-list/fight-list.component.ts
+++ frontend/src/app/views/fight-list/fight-list.component.ts
@@ -605,23 +605,23 @@ export class FightListComponent extends RbacBasedComponent implements OnInit, On
     //Put default points.
     if (duel.competitor1 !== null && duel.competitor2 == null) {
       duel.competitor1Score = [];
-      duel.competitor1Score.push(Score.IPPON);
-      duel.competitor1Score.push(Score.IPPON);
+      duel.competitor1Score.push(Score.FUSEN_GACHI);
+      duel.competitor1Score.push(Score.FUSEN_GACHI);
     } else if (duel.competitor2 !== null && duel.competitor1 == null) {
       duel.competitor2Score = [];
-      duel.competitor2Score.push(Score.IPPON);
-      duel.competitor2Score.push(Score.IPPON);
+      duel.competitor2Score.push(Score.FUSEN_GACHI);
+      duel.competitor2Score.push(Score.FUSEN_GACHI);
     }
   }
 
   removeIpponScores(duel: Duel): void {
     for (let i: number = 0; i < duel.competitor1Score.length; i++) {
-      if (duel.competitor1Score[i] == Score.IPPON) {
+      if (duel.competitor1Score[i] == Score.FUSEN_GACHI) {
         duel.competitor1Score = [];
       }
     }
     for (let i: number = 0; i < duel.competitor2Score.length; i++) {
-      if (duel.competitor2Score[i] == Score.IPPON) {
+      if (duel.competitor2Score[i] == Score.FUSEN_GACHI) {
         duel.competitor2Score = [];
       }
     }
