311bc9bcb3b931b429eeecfc0d3b9cfd8648394e
Software Magico <softwaremagico@gmail.com>
2024-05-18 09:10:08 +0200

    Changed table border color

--- backend/kendo-tournament-pdf/src/test/java/com/softwaremagico/kt/pdf/ListTests.java
+++ backend/kendo-tournament-pdf/src/test/java/com/softwaremagico/kt/pdf/ListTests.java
@@ -10,12 +10,12 @@ package com.softwaremagico.kt.pdf;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -30,10 +30,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Locale;
 
@@ -41,13 +45,18 @@ import java.util.Locale;
 @Test(groups = {"scoreListPdf"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ListTests extends BasicDataTest {
-    private static final String PDF_PATH_OUTPUT = System.getProperty("java.io.tmpdir") + File.separator;
+    private static final String PDF_PATH_OUTPUT = System.getProperty("java.io.tmpdir") + File.separator + "PdfTests" + File.separator;
 
     @Autowired
     private PdfController pdfController;
 
     @Autowired
     private RankingController rankingController;
+
+    @BeforeClass
+    public void prepareFolder() throws IOException {
+        Files.createDirectories(Paths.get(PDF_PATH_OUTPUT));
+    }
 
     @BeforeClass
     public void prepareData() {
@@ -91,5 +100,10 @@ public class ListTests extends BasicDataTest {
     public void generateFightSummaryPdf() {
         Assert.assertEquals(pdfController.generateFightsSummaryList(Locale.getDefault(), tournament)
                 .createFile(PDF_PATH_OUTPUT + "FightSummaryList.pdf"), 2); // No clue why are 2 pages and not 1.
+    }
+
+    @AfterClass
+    public void removeFolder() {
+        Assert.assertTrue(deleteDirectory(new File(PDF_PATH_OUTPUT)));
     }
 }
