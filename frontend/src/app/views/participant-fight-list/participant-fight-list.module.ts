6823d22224b8dacb846e25f85fbfaeb0b1fd2526
Software Magico <softwaremagico@gmail.com>
2024-05-19 07:49:58 +0200

    New view structure

---
+++ frontend/src/app/views/participant-fight-list/participant-fight-list.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ParticipantFightListComponent} from './participant-fight-list.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSpinnerOverlayModule} from "../../components/mat-spinner-overlay/mat-spinner-overlay.module";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {RbacModule} from "../../pipes/rbac-pipe/rbac.module";
+import {TranslateModule} from "@ngx-translate/core";
+import {FightModule} from "../../components/fight/fight.module";
+import {UntieFightModule} from "../../components/untie-fight/untie-fight.module";
+import {FilterModule} from "../../components/basic/filter/filter.module";
+
+
+@NgModule({
+  declarations: [
+    ParticipantFightListComponent
+  ],
+  exports: [ParticipantFightListComponent],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSpinnerOverlayModule,
+    MatTooltipModule,
+    RbacModule,
+    TranslateModule,
+    FightModule,
+    UntieFightModule,
+    FilterModule
+  ]
+})
+export class ParticipantFightListModule {
+}
