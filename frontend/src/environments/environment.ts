462d986c04153a21d240430b72e887bb32c22b05
Software Magico <softwaremagico@gmail.com>
2024-12-22 11:24:09 +0100

    Fixed docker variable replace.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   backendUrl: "http://localhost:8080/kendo-tournament-backend",
   websocketsUrl: "ws://localhost:8080/kendo-tournament-backend/websockets",
   websocketsTopicPrefix: "/topic",
-  achievementsEnabled: "true",
+  achievementsEnabled: true,
 };
