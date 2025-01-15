f1c6ec570614b09d99011edb40ddf4e5b0a2a810
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-01-15 16:51:42 +0100

    Updated copyright

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/controller/models/TournamentExtraPropertyDTO.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/controller/models/TournamentExtraPropertyDTO.java
@@ -4,7 +4,7 @@ package com.softwaremagico.kt.core.controller.models;
  * #%L
  * Kendo Tournament Manager (Rest)
  * %%
- * Copyright (C) 2021 - 2024 Softwaremagico
+ * Copyright (C) 2021 - 2025 Softwaremagico
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
