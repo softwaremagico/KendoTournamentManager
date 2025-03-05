8d1f6e28843e69d222303b3232c850ae15e7b14d
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-03-05 16:29:17 +0100

    Option for disabling new version check

--- frontend/src/environments/environment.docker.ts
+++ frontend/src/environments/environment.docker.ts
@@ -4,4 +4,5 @@ export const environment = {
   websocketsUrl: "DOCKER:WEBSOCKET_PROTOCOL://DOCKER:MACHINE_DOMAIN/kendo-tournament-backend/websockets",
   websocketsTopicPrefix: "/topic",
   achievementsEnabled: "DOCKER:ACHIEVEMENTS_ENABLED",
+  checkForNewVersion: "DOCKER:CHECK_FOR_NEW_VERSION",
 };
