8cca56e3e829dd2b3fdabdacf11c69b041bca1d6
Software Magico <softwaremagico@gmail.com>
2025-04-03 15:54:49 +0200

    Fixing Sonarqube recommendations.

--- backend/kendo-tournament-pdf/src/test/java/com/softwaremagico/kt/html/HtmlTests.java
+++ backend/kendo-tournament-pdf/src/test/java/com/softwaremagico/kt/html/HtmlTests.java
@@ -10,12 +10,12 @@ package com.softwaremagico.kt.html;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -30,7 +30,6 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.util.Locale;
 
 @SpringBootTest
