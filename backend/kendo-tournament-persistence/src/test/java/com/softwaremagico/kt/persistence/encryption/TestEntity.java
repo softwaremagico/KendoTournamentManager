79be91508c1d44ce9f69a0eb7f1c0cbd464a5b7a
Software Magico <softwaremagico@gmail.com>
2025-10-13 14:08:17 +0200

    Removing tests

--- backend/kendo-tournament-persistence/src/test/java/com/softwaremagico/kt/persistence/encryption/TestEntity.java
+++ backend/kendo-tournament-persistence/src/test/java/com/softwaremagico/kt/persistence/encryption/TestEntity.java
@@ -10,12 +10,12 @@ package com.softwaremagico.kt.persistence.encryption;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -34,7 +34,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.Random;
 
 @Entity
 public class TestEntity {
@@ -176,15 +175,15 @@ public class TestEntity {
     }
 
     private long generateRandomLong() {
-        return new Random().nextLong();
+        return random.nextLong();
     }
 
     public Integer generateRandomInteger() {
-        return new Random().nextInt();
+        return random.nextInt();
     }
 
     public Double generateRandomDouble() {
-        return new Random().nextDouble();
+        return random.nextDouble();
     }
 
     @Override
