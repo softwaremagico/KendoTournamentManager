f44da989a7d0b6f064232738b23cc90fe5e9d62b
Software Magico <softwaremagico@gmail.com>
2025-10-16 09:06:37 +0200

    Removed duplicated

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/controller/models/ParticipantDTO.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/controller/models/ParticipantDTO.java
@@ -42,9 +42,6 @@ public class ParticipantDTO extends ElementDTO implements IParticipantName, IAut
 
     private static final int HASH_VALUE = 31;
 
-    @Serial
-    private static final long serialVersionUID = -646370421493264121L;
-
     private String idCard;
 
     @NotBlank
