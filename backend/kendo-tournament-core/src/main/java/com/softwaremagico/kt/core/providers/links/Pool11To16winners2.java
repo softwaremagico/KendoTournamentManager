5d77c71f5c7b6ba7b9ec5ff67d8edc108970122b
Software Magico <softwaremagico@gmail.com>
2025-10-30 13:49:41 +0100

    Fixed 11 pool relations.

--- backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/providers/links/Pool11To16winners2.java
+++ backend/kendo-tournament-core/src/main/java/com/softwaremagico/kt/core/providers/links/Pool11To16winners2.java
@@ -21,34 +21,44 @@ package com.softwaremagico.kt.core.providers.links;
  * #L%
  */
 
-public enum Pool6to8winners2 {
+public enum Pool11To16winners2 {
     WINNER_1A(0, 0, 0),
     WINNER_1B(1, 0, 6),
-    WINNER_2A(0, 1, 2),
-    WINNER_2B(1, 1, 5),
-    WINNER_3A(0, 2, 3),
-    WINNER_3B(1, 2, 5),
-    WINNER_4A(0, 3, 4),
-    WINNER_4B(1, 3, 2),
-    WINNER_5A(0, 4, 6),
-    WINNER_5B(1, 4, 1),
-    WINNER_6A(0, 5, 7),
-    WINNER_6B(1, 5, 1);
+    WINNER_2A(0, 1, 1),
+    WINNER_2B(1, 1, 7),
+    WINNER_3A(0, 2, 2),
+    WINNER_3B(1, 2, 8),
+    WINNER_4A(0, 3, 3),
+    WINNER_4B(1, 3, 9),
+    WINNER_5A(0, 4, 4),
+    WINNER_5B(1, 4, 10),
+    WINNER_6A(0, 5, 5),
+    WINNER_6B(1, 5, 11),
+    WINNER_7A(0, 6, 11),
+    WINNER_7B(1, 6, 6),
+    WINNER_8A(0, 7, 12),
+    WINNER_8B(1, 7, 7),
+    WINNER_9A(0, 8, 13),
+    WINNER_9B(1, 8, 8),
+    WINNER_10A(0, 9, 14),
+    WINNER_10B(1, 9, 9),
+    WINNER_11A(0, 10, 15),
+    WINNER_11B(1, 10, 10);
 
     private final int winner;
     private final int source;
     private final int destination;
 
-    Pool6to8winners2(int winner, int source, int destination) {
+    Pool11To16winners2(int winner, int source, int destination) {
         this.winner = winner;
         this.source = source;
         this.destination = destination;
     }
 
     public static int getDestination(int sourceGroupLevelIndex, int winnerOrder) {
-        for (Pool6to8winners2 pool6To8Winners2 : Pool6to8winners2.values()) {
-            if (pool6To8Winners2.winner == winnerOrder && pool6To8Winners2.source == sourceGroupLevelIndex) {
-                return pool6To8Winners2.destination;
+        for (Pool11To16winners2 pool11To16winners2 : Pool11To16winners2.values()) {
+            if (pool11To16winners2.winner == winnerOrder && pool11To16winners2.source == sourceGroupLevelIndex) {
+                return pool11To16winners2.destination;
             }
         }
         return -1;
