622dda6a99d959fe00710b6a88aa901856836a71
Software Magico <softwaremagico@gmail.com>
2026-06-26 12:51:39 +0200

    feat: add tests for Swiss tournament with 10 and 16 teams

---
+++ backend/kendo-tournament-core/src/test/java/com/softwaremagico/kt/core/tests/tournament/SwissTournament16TeamsTest.java
@@ -0,0 +1,226 @@
+package com.softwaremagico.kt.core.tests.tournament;
+
+/*-
+ * #%L
+ * Kendo Tournament Manager (Core)
+ * %%
+ * Copyright (C) 2021 - 2026 Softwaremagico
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import com.softwaremagico.kt.core.controller.ClubController;
+import com.softwaremagico.kt.core.controller.DuelController;
+import com.softwaremagico.kt.core.controller.FightController;
+import com.softwaremagico.kt.core.controller.GroupController;
+import com.softwaremagico.kt.core.controller.ParticipantController;
+import com.softwaremagico.kt.core.controller.RoleController;
+import com.softwaremagico.kt.core.controller.TeamController;
+import com.softwaremagico.kt.core.controller.TournamentController;
+import com.softwaremagico.kt.core.controller.models.ClubDTO;
+import com.softwaremagico.kt.core.controller.models.FightDTO;
+import com.softwaremagico.kt.core.controller.models.ParticipantDTO;
+import com.softwaremagico.kt.core.controller.models.RoleDTO;
+import com.softwaremagico.kt.core.controller.models.TeamDTO;
+import com.softwaremagico.kt.core.controller.models.TournamentDTO;
+import com.softwaremagico.kt.core.converters.FightConverter;
+import com.softwaremagico.kt.core.converters.TournamentConverter;
+import com.softwaremagico.kt.core.converters.models.FightConverterRequest;
+import com.softwaremagico.kt.core.managers.TeamsOrder;
+import com.softwaremagico.kt.core.providers.RankingProvider;
+import com.softwaremagico.kt.core.score.ScoreOfTeam;
+import com.softwaremagico.kt.persistence.entities.Fight;
+import com.softwaremagico.kt.persistence.entities.Group;
+import com.softwaremagico.kt.persistence.values.RoleType;
+import com.softwaremagico.kt.persistence.values.Score;
+import com.softwaremagico.kt.persistence.values.TournamentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootTest
+@Test(groups = {"swissTournament16Test"})
+public class SwissTournament16TeamsTest extends AbstractTestNGSpringContextTests {
+
+    private static final String CLUB_NAME = "Swiss16Club";
+    private static final String CLUB_CITY = "Swiss16City";
+    private static final int MEMBERS = 3;
+    private static final int TEAMS = 16;
+    private static final int ROUNDS = 4;
+    private static final int FIGHTS_PER_ROUND = 8;
+    private static final String TOURNAMENT_NAME = "SwissTournament16TeamsTest";
+
+    @Autowired
+    private TournamentController tournamentController;
+
+    @Autowired
+    private TournamentConverter tournamentConverter;
+
+    @Autowired
+    private ParticipantController participantController;
+
+    @Autowired
+    private RoleController roleController;
+
+    @Autowired
+    private TeamController teamController;
+
+    @Autowired
+    private ClubController clubController;
+
+    @Autowired
+    private RankingProvider rankingProvider;
+
+    @Autowired
+    private GroupController groupController;
+
+    @Autowired
+    private FightController fightController;
+
+    @Autowired
+    private DuelController duelController;
+
+    @Autowired
+    private FightConverter fightConverter;
+
+    private ClubDTO clubDTO;
+    private TournamentDTO tournamentDTO;
+
+    @Test
+    public void addClub() {
+        clubDTO = clubController.create(new ClubDTO(CLUB_NAME, CLUB_CITY), null, null);
+    }
+
+    @Test(dependsOnMethods = "addClub")
+    public void addParticipants() {
+        for (int i = 0; i < MEMBERS * TEAMS; i++) {
+            participantController.create(new ParticipantDTO(String.format("S16-%04d", i),
+                    String.format("name%s", i), String.format("lastname%s", i), clubDTO), null, null);
+        }
+    }
+
+    @Test(dependsOnMethods = "addParticipants")
+    public void addTournament() {
+        Assert.assertEquals(tournamentController.count(), 0);
+        final TournamentDTO newTournament = new TournamentDTO(TOURNAMENT_NAME, 1, MEMBERS, TournamentType.SWISS);
+        tournamentDTO = tournamentController.create(newTournament, null, null);
+        Assert.assertEquals(tournamentController.count(), 1);
+    }
+
+    @Test(dependsOnMethods = "addTournament")
+    public void addRoles() {
+        for (ParticipantDTO competitor : participantController.get()) {
+            roleController.create(new RoleDTO(tournamentDTO, competitor, RoleType.COMPETITOR), null, null);
+        }
+        Assert.assertEquals(roleController.count(tournamentDTO), participantController.count());
+    }
+
+    @Test(dependsOnMethods = "addRoles")
+    public void addTeams() {
+        int teamIndex = 0;
+        TeamDTO team = null;
+        int teamMember = 0;
+
+        final List<Group> groups = groupController.getGroups(tournamentDTO, 0);
+        Assert.assertEquals(groups.size(), 1);
+
+        for (ParticipantDTO competitor : participantController.get()) {
+            if (team == null) {
+                teamIndex++;
+                team = new TeamDTO("Team" + String.format("%02d", teamIndex), tournamentDTO);
+                teamMember = 0;
+            }
+
+            team.addMember(competitor);
+            team = teamController.create(team, null, null);
+
+            if (teamMember == 0) {
+                groupController.addTeams(groups.getFirst().getId(), Collections.singletonList(team), null, null);
+            }
+
+            teamMember++;
+            if (teamMember >= MEMBERS) {
+                team = null;
+            }
+        }
+
+        Assert.assertEquals(teamController.count(tournamentDTO), TEAMS);
+        Assert.assertEquals(groupController.getGroups(tournamentDTO, 0).getFirst().getTeams().size(), TEAMS);
+    }
+
+    @Test(dependsOnMethods = "addTeams")
+    public void createAndSolveSwissRoundsWithoutByes() {
+        for (int level = 0; level < ROUNDS; level++) {
+            final int roundLevel = level;
+            final List<FightDTO> createdFights = fightController.createFights(tournamentDTO.getId(), TeamsOrder.NONE, level, null, null);
+            Assert.assertEquals(createdFights.size(), FIGHTS_PER_ROUND);
+
+            final Group group = groupController.getGroups(tournamentDTO, 0).getFirst();
+            final List<Fight> fightsInRound = group.getFights().stream().filter(fight -> fight.getLevel() == roundLevel).toList();
+            Assert.assertEquals(fightsInRound.size(), FIGHTS_PER_ROUND);
+
+            for (Fight fight : fightsInRound) {
+                Assert.assertNotNull(fight.getTeam1());
+                Assert.assertNotNull(fight.getTeam2());
+                fight.getDuels().getFirst().addCompetitor1Score(Score.MEN);
+                fight.getDuels().getFirst().addCompetitor1Score(Score.MEN);
+                fight.getDuels().forEach(duel -> duel.setFinished(true));
+                fightController.update(fightConverter.convert(new FightConverterRequest(fight)), null, null);
+            }
+
+            final Group updatedGroup = groupController.getGroups(tournamentDTO, 0).getFirst();
+            Assert.assertEquals(updatedGroup.getFights().stream().filter(fight -> fight.getLevel() == roundLevel).count(),
+                    FIGHTS_PER_ROUND);
+        }
+    }
+
+    @Test(dependsOnMethods = "createAndSolveSwissRoundsWithoutByes")
+    public void checkFinalRanking() {
+        final List<ScoreOfTeam> ranking = rankingProvider.getTeamsScoreRanking(tournamentConverter.reverse(tournamentDTO));
+        Assert.assertEquals(ranking.size(), TEAMS);
+        Assert.assertNotNull(ranking.getFirst().getTeam());
+        Assert.assertNotNull(ranking.getFirst().getTeam().getName());
+
+        final Group group = groupController.getGroups(tournamentDTO, 0).getFirst();
+        Assert.assertEquals(group.getFights().size(), ROUNDS * FIGHTS_PER_ROUND);
+        Assert.assertTrue(group.getFights().stream().allMatch(fight -> fight.getTeam1() != null && fight.getTeam2() != null));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void deleteTournament() {
+        if (tournamentDTO != null) {
+            groupController.delete(tournamentDTO);
+            fightController.delete(tournamentDTO);
+            duelController.delete(tournamentDTO);
+            teamController.delete(tournamentDTO);
+            roleController.delete(tournamentDTO);
+            tournamentController.delete(tournamentDTO, null, null);
+        }
+        participantController.deleteAll();
+        if (clubDTO != null) {
+            clubController.delete(clubDTO, null, null);
+        }
+        Assert.assertEquals(fightController.count(), 0);
+        Assert.assertEquals(duelController.count(), 0);
+    }
+}
+
+
