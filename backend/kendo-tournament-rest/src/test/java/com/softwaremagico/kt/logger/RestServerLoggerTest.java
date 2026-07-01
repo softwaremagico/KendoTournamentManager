487a6e32f8f6d5a7ab0348ea1e065a939948a97c
Software Magico <softwaremagico@gmail.com>
2026-07-01 17:14:32 +0200

    refactor(statistics): enhance score handling and simplify duration calculations in ParticipantFightStatisticsProvider

---
+++ backend/kendo-tournament-rest/src/test/java/com/softwaremagico/kt/logger/RestServerLoggerTest.java
@@ -0,0 +1,80 @@
+package com.softwaremagico.kt.logger;
+
+/*-
+ * #%L
+ * Kendo Tournament Manager (Rest)
+ * %%
+ * Copyright (C) 2021 - 2026 Softwaremagico
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class RestServerLoggerTest {
+
+    private Logger logger;
+    private ListAppender<ILoggingEvent> appender;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        logger = (Logger) LoggerFactory.getLogger(RestServerLogger.class);
+        logger.setLevel(Level.DEBUG);
+        appender = new ListAppender<>();
+        appender.start();
+        logger.addAppender(appender);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        logger.detachAppender(appender);
+    }
+
+    @Test(groups = "restServicesUnit")
+    public void shouldLogSevereWithClassPrefix() {
+        RestServerLogger.severe(getClass(), "http {}", 500);
+
+        ILoggingEvent event = appender.list.get(appender.list.size() - 1);
+        assertEquals(event.getLevel(), Level.ERROR);
+        assertTrue(event.getFormattedMessage().contains(getClass().getName() + ": http 500"));
+    }
+
+    @Test(groups = "restServicesUnit")
+    public void shouldLogErrorTemplate() {
+        RestServerLogger.errorMessage(getClass(), "bad request {}", 400);
+
+        ILoggingEvent event = appender.list.get(appender.list.size() - 1);
+        assertEquals(event.getLevel(), Level.ERROR);
+        assertTrue(event.getFormattedMessage().contains(getClass().getName() + ": bad request 400"));
+    }
+
+    @Test(groups = "restServicesUnit")
+    public void shouldReportDebugEnabled() {
+        logger.setLevel(Level.DEBUG);
+
+        assertTrue(RestServerLogger.isDebugEnabled());
+    }
+}
+
