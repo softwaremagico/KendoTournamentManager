825f756db9d3c854a9b25989002afa7e974855b0
Software Magico <softwaremagico@gmail.com>
2026-07-24 18:13:14 +0200

    fix(): sonarqube issues

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -51,6 +51,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
